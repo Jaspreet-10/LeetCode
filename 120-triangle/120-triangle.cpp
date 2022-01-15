28ae8c445baccc8c3874009c7ef70a3c0483316d
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-01-15 12:18:03 +0530

    Time: 6 ms (65.59%), Space: 8.6 MB (87.34%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+     vector<int>v;
+        int row=triangle.size(),sum=0;;
+        for(int i=0;i<triangle[row-1].size();++i){
+            v.push_back(triangle[row-1][i]);
+        }
+        for(int i=row-2;i>=0;--i){
+            int mini=INT_MAX;
+            for(int j=0;j<triangle[i].size();++j){
+                v[j]=min(v[j]+triangle[i][j],v[j+1]+triangle[i][j]);
+            }
+        }
+        return v[0];
+    }
+};
