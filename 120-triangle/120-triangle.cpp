b7d26862eb972d592540a6d074e9ef8657f931e0
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-01-15 12:19:18 +0530

    Time: 18 ms (5.69%), Space: 8.7 MB (58.28%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -7,7 +7,6 @@ public:
             v.push_back(triangle[row-1][i]);
         }
         for(int i=row-2;i>=0;--i){
-            int mini=INT_MAX;
             for(int j=0;j<triangle[i].size();++j){
                 v[j]=min(v[j]+triangle[i][j],v[j+1]+triangle[i][j]);
             }
