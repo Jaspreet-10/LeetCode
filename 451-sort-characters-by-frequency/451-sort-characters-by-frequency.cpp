fc0c406fac2d48948a4e7b21e64b7333b5131093
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-01-22 17:04:48 +0530

    Time: 28 ms (16.28%), Space: 8.9 MB (20.16%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -4,34 +4,19 @@ public:
         multimap<int,string>m;
         sort(s.begin(),s.end());
         string str="";
-        int len=0;
-        for(int i=0;i<s.length()-1;++i){
-            if(s[i]==s[i+1]){
+        int len=0 , n=s.length();
+        for(int i=0;i<n;++i){
+            if(i!=n-1 and s[i]==s[i+1]){
                 str+=s[i];
                 len=str.length();
             }else{
                 str+=s[i];
                 m.insert(pair<int,string>(len,str));
-                // if(len>=1){
-                // str+=s[i];
-                // m.insert(pair<int,string>(len,str));
-                // }
-                // else{
-                //     str+=s[i];
-                //     m.insert(pair<int,string>(0,str));
-                // }
                 len=0;
                 str="";
             }
         }
-        str+=s[s.length()-1];
-        if(str.length()>1){
-        m.insert(pair<int,string>(str.length()-1,str));
-        }
-        else
-        m.insert(pair<int,string>(0,str));
-        str="";
-        for(auto it=m.rbegin();it!=m.rend();it++){
+            for(auto it=m.rbegin();it!=m.rend();it++){
             str+=it->second;
         }
         return str;
