bb34642fac3935209afb0343b644bb108045be26
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-01-27 16:19:49 +0530

    Time: 43 ms (82.35%), Space: 13.2 MB (50.56%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,59 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& num, int target) {
-         vector<vector<int> > res;
-        
-        if (num.empty())
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+         vector<vector<int>>res;
+        if(nums.size()<4)
             return res;
-        int n = num.size(); 
-        sort(num.begin(),num.end());
-    
-        for (int i = 0; i < n; i++) {
-        
-            int target_3 = target - num[i];
-        
-            for (int j = i + 1; j < n; j++) {
-            
-                int target_2 = target_3 - num[j];
-            
-                int front = j + 1;
-                int back = n - 1;
-            
-                while(front < back) {
-                
-                    int two_sum = num[front] + num[back];
-                
-                    if (two_sum < target_2) front++;
-                
-                    else if (two_sum > target_2) back--;
-                
-                    else {
-                    
-                        vector<int> quadruplet(4, 0);
-                        quadruplet[0] = num[i];
-                        quadruplet[1] = num[j];
-                        quadruplet[2] = num[front];
-                        quadruplet[3] = num[back];
-                        res.push_back(quadruplet);
-                    
-                        // Processing the duplicates of number 3
-                        while (front < back && num[front] == quadruplet[2]) ++front;
-                    
-                        // Processing the duplicates of number 4
-                        while (front < back && num[back] == quadruplet[3]) --back;
-                
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n-3;++i){
+            for(int j=i+1;j<n-2;++j){
+                int diff=target-nums[j]-nums[i];
+                int left=j+1,right=n-1;
+                while(left<right){
+                    int sum=nums[left]+nums[right];
+                    if(sum<diff)left++;
+                    else if(sum>diff) right--;
+                    else{
+                        vector<int>v(4,0);
+                        v[0]=nums[i];
+                        v[1]=nums[j];
+                        v[2]=nums[left];
+                        v[3]=nums[right];
+                        res.push_back(v);
+                        while(left<right and v[2]==nums[left])left++;
+                        while(left<right and v[3]==nums[right])right--;
                     }
                 }
-                
-                // Processing the duplicates of number 2
-                while(j + 1 < n && num[j + 1] == num[j]) ++j;
+                while(j+1<n and nums[j+1]==nums[j])j++;
             }
-        
-            // Processing the duplicates of number 1
-            while (i + 1 < n && num[i + 1] == num[i]) ++i;
-        
+            while(i+1<n and nums[i+1]==nums[i])i++;
         }
-    
         return res;
-    }    
+    }
 };
