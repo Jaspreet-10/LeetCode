de45d877ef346bc12be474582a0c6b6e68836e54
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-02-03 14:37:50 +0530

    Time: 17 ms (5.08%), Space: 12.5 MB (29.19%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
+    int helper(vector<int>nums,int low,int high,int target){
+        int mid=(low+high)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(low>high)
+            return -1;
+        if(nums[mid]>target){
+            return helper(nums,low,mid-1,target);
+        }
+        else{
+            return helper(nums,mid+1,high,target);
+        }
+       return -1;
+    }
     int search(vector<int>& nums, int target) {
-        if(nums.size()==1&&nums[0]==target){
-            return 0;
+        int i=0,j=nums.size()-1;
+        while(nums[i]>nums[j]){
+            i++;
         }
-        vector<int>::iterator it;
-        it = std::find (nums.begin(), nums.end(), target);
-        if(it!=nums.end()){
-            return (it-nums.begin());
+        int ans=-1;
+        if(nums[i]==target) return i;
+        if(target>=nums[i] and target<=nums[j]){
+        ans=helper(nums,i,j,target);
         }
-        return -1;
+        else{
+           ans=helper(nums,0,i,target);
+        }
+        return ans;
     }
 };
