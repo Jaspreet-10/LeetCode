ceb55373bc3081b35114a5baa08a2de83fdf75a7
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-02-03 17:27:02 +0530

    Time: 374 ms (17.43%), Space: 49.4 MB (13.23%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-         vector<vector<int>>res;
-        if(nums.size()<4)
-            return res;
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
+    vector<vector<int>> fourSum(vector<int>& arr, int k) {
+        vector<vector<int> >res;
+        map<vector<int>,int>m;
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
         for(int i=0;i<n-3;++i){
             for(int j=i+1;j<n-2;++j){
-                int diff=target-nums[j]-nums[i];
+                int diff=k-(arr[i]+arr[j]);
                 int left=j+1,right=n-1;
                 while(left<right){
-                    int sum=nums[left]+nums[right];
-                    if(sum<diff)left++;
-                    else if(sum>diff) right--;
+                    int sum=arr[left]+arr[right];
+                    if(sum<diff)
+                    left++;
+                    else if(sum>diff)
+                    right--;
                     else{
-                        vector<int>v(4,0);
-                        v[0]=nums[i];
-                        v[1]=nums[j];
-                        v[2]=nums[left];
-                        v[3]=nums[right];
-                        res.push_back(v);
-                        while(left<right and v[2]==nums[left])left++;
-                        while(left<right and v[3]==nums[right])right--;
+                        vector<int>v={arr[i],arr[j],arr[left],arr[right]};
+                        left++;
+                        right--;
+                        if(m[v]==0){
+                         res.push_back(v);
+                         m[v]++;
+                        }
                     }
                 }
-                while(j+1<n and nums[j+1]==nums[j])j++;
             }
-            while(i+1<n and nums[i+1]==nums[i])i++;
         }
-        return res;
+      return  res;
     }
 };
