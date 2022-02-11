51d29f93dac369fd35e1bedbc8dd2b23449c0d1a
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-02-11 09:21:35 +0530

    Time: 14 ms (44.26%), Space: 7.2 MB (89.63%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int>m1(26,0),m2(26,0);
-        int size1=s1.length(),size2=s2.length(),right=0,left=0;
-        if(size2<size1)
+        if(s1.length()>s2.length())
             return false;
-        for(int i=0;i<size1;++i) {
-            m1[s1[i]-'a']+=1;
-            m2[s2[i]-'a']+=1;
+        vector<int>arr1(26,0),arr2(26,0);
+        int right=0,left=0,size=s2.length();
+        for(int i=0;i<s1.length();++i){
+            arr1[s1[i]-'a']+=1;
+            arr2[s2[i]-'a']+=1;
             right++;
         }
         right-=1;
-        while(right<size2){
-            if(m1==m2) 
-                return true;
+        while(right<size){
+            if(arr1==arr2) return true;
             right+=1;
-            if(right!=size2)
-                m2[s2[right]-'a']++;
-                m2[s2[left]-'a']--;
-                left+=1;
-        }  
+            if(right<size)
+                arr2[s2[right]-'a']+=1;
+                arr2[s2[left]-'a']-=1;
+            left+=1;
+        }
         return false;
     }
 };
