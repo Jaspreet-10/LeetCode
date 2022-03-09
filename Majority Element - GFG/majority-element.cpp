c7fff704caa2f61d61f34a24d4245930e2d0dcd5
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-03-09 11:24:37 +0530

    Added solution - LeetHub

--- Majority Element - GFG/majority-element.cpp
+++ Majority Element - GFG/majority-element.cpp
@@ -10,18 +10,43 @@ using namespace std;
 
 class Solution{
   public:
-     // Function to find majority element in the array
-    // a: input array
-    // size: size of input array
+int findCandidate(int a[], int size) 
+{ 
+	int maj_index = 0, count = 1; 
+	for (int i = 1; i < size; i++) 
+	{ 
+		if (a[maj_index] == a[i]) 
+			count++; 
+		else
+			count--; 
+		if (count == 0) 
+		{ 
+			maj_index = i; 
+			count = 1; 
+		} 
+	} 
+	return a[maj_index]; 
+} 
+
+/* Function to check if the candidate 
+occurs more than n/2 times */
+int isMajority(int a[], int size, int cand) 
+{ 
+	int count = 0; 
+	for (int i = 0; i < size; i++){ 
+	
+	if (a[i] == cand) 
+	count++; 
+		
+	if (count > size/2) 
+	return a[i]; 
+	}
+	return -1; 
+}
     int majorityElement(int a[], int size)
     {
-       unordered_map<int,int>m;
-        for(int i=0;i<size;++i){
-            m[a[i]]++;
-            if(m[a[i]]!=0 and m[a[i]]>(size/2))
-                return a[i];
-        }
-        return -1;
+       int cand=findCandidate(a,size);
+       return isMajority(a,size,cand);
     }
 };
 
