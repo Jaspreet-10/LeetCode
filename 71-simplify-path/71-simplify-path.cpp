68f815ba73b55eae9e5001a3af4605d1d7c9f054
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-03-14 10:48:43 +0530

    Time: 3 ms (95.14%), Space: 10.7 MB (18.17%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+    vector<string> result;
+    stack<string>s;
+    std::string delimiter = "/";
+    size_t pos = 0;
+    std::string token;
+    auto start = 0U;
+    auto end = path.find(delimiter);
+    while (end != std::string::npos)
+    {
+        result.push_back(path.substr(start, end - start));
+        start = end + delimiter.length();
+        end = path.find(delimiter, start);
+    }
+        result.push_back(path.substr(start, end));
+        for(int i=0;i<result.size();++i){
+            // cout<<result[i]<<" ";
+            if(!s.empty() and result[i]=="..") s.pop();
+            else if(result[i]!="" and result[i]!="." and result[i]!=".."){
+                s.push(result[i]);
+            }
+        }
+        int size=s.size();
+        string str="";
+        if(s.empty()) return "/";
+        for(int i=0;i<size;++i){
+            // cout<<s.top()<<" ";
+            str.insert(0,("/"+s.top()));
+            s.pop();
+        }
+        return str;
+    }
+};
