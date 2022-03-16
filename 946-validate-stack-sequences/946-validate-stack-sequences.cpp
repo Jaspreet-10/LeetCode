49e169d8fc215bec7b03a205902f5f71add8b016
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-03-16 08:59:56 +0530

    Time: 8 ms (83.81%), Space: 15.3 MB (59.16%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -3,7 +3,7 @@ public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         int i=0,j=0,len=pushed.size();
         stack<int>st;
-        while(i<len||j<len){
+        while(i<len){
             if(i<len){
                 st.push(pushed[i]);
                 ++i;
@@ -12,9 +12,9 @@ public:
                 ++j;
                 st.pop();
             }
-            if(!st.empty() and popped[j]!=st.top() and i==len)
-                return false;
+            // if(!st.empty() and popped[j]!=st.top() and i==len)
+            //     return false;
         }
-        return true;
+        return st.empty();
     }
 };
