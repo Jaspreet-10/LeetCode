67404698d775ba8b9eb6db9d7f56a0b540bfe0d5
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-03-21 10:59:15 +0530

    Time: 13 ms (7.37%), Space: 12.2 MB (6.29%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProfit(int index,vector<int>nums,int n,unordered_map<int,int>&memo){
+        
+        if(index>=n){
+            return 0;
+        }
+        int currentKey = index+1;
+        if(memo.find(currentKey)!=memo.end())
+            return memo[currentKey];
+        
+        int robbed=nums[index]+maxProfit(index+2,nums,n,memo);
+        
+        int notRobbed=maxProfit(index+1,nums,n,memo);
+        
+        memo[currentKey]=max(robbed,notRobbed);
+        return memo[currentKey];
+    }
+    int rob(vector<int>& nums) {
+        unordered_map<int,int>memo;
+        unordered_map<int,int>memo1;
+        int n=nums.size();
+        if(n==1)
+            return nums[n-1];
+        if(n==2)
+            return max(nums[0],nums[1]);
+        return max(maxProfit(0,nums,n-1,memo),maxProfit(1,nums,n,memo1));  
+    }
+};
