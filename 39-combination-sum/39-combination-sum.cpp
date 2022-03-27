99a53e155222531c3e3cebcdfc750700ec6ca7a8
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-03-27 18:05:58 +0530

    Time: 1631 ms (5.04%), Space: 326.6 MB (5.05%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     vector<vector<int>>res;
-    void uniqueCombinations(int index,int target,vector<int>c,vector<int>ans){
-        if(index==c.size()){
-            if(target==0)
-            res.push_back(ans);
-            return ;
-        }
-        if(c[index]<=target){
-        ans.push_back(c[index]);
-        uniqueCombinations(index,target-c[index],c,ans);
-        ans.erase(ans.begin()+ans.size()-1);
-        }
-        uniqueCombinations(index+1,target,c,ans);
+vector<vector<int>> uniqueCombinations(int index,int target,vector<int>& candidates, vector<int>&v){
+    if(target==0){
+        res.push_back(v);
+        return res;
     }
+    if(index>=candidates.size())
+        return res;
+    // pair<vector<int>,int> currentKey = {v,target};
+    // if(memo.find(currentKey)!=memo.end())
+    //     return res;
+    int consider=0;
+    if(candidates[index]<=target){
+        v.push_back(candidates[index]);
+        uniqueCombinations(index,target-candidates[index],candidates,v);
+        v.pop_back();
+    }
+    uniqueCombinations(index+1,target,candidates,v);
+    // memo[currentKey] = res;
+    return res;
+}
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         if(candidates.empty()) return res;
         vector<int>ds;
-        uniqueCombinations(0,target,candidates,ds);
-        return res;
+        // map<pair<vector<int>,int>,vector<vector<int>>>memo;
+        return uniqueCombinations(0,target,candidates,ds);
     }
 };
