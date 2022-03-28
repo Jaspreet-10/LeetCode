200f483108e9c4abcd907daf4e35e6194d3fc3e8
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-03-28 09:15:58 +0530

    Time: 8 ms (60.28%), Space: 15 MB (32.08%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
+    bool biSearch(int low,int high,vector<int>nums,int target){
+        if(low<high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target)
+                return true;
+            if(nums[mid] > target)
+                return biSearch(low,mid,nums,target);
+            else
+              return biSearch(mid+1,nums.size(),nums,target);
+        }
+        return false;
+    }
     bool search(vector<int>& nums, int target) {
-        auto it=find(nums.begin(),nums.end(),target);
-        if(it!=nums.end())
-            return true;
+        int i=0,n=nums.size();
+        for(i=0;i<n-1;++i){
+            if(nums[i] == target)
+                return true;
+            if(nums[i+1]<nums[i])
+                break;
+        }
+            if(nums[i] == target)
+                return true;
+        
+            else if(target<nums[i])
+                return biSearch(i+1,n,nums,target);
+            else
+                return biSearch(0,i,nums,target);
         return false;
     }
 };
