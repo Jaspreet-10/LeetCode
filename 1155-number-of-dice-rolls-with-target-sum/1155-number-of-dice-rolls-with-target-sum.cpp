b1bbc85d02b9f3dc0f60e18540032180ae1f19b0
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-03-29 21:55:46 +0530

    Time: 42 ms (54.10%), Space: 8.4 MB (46.89%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+//     TC ->O(n*target*f)
+//     SC -> O(n*target)
     int totalWays(int n,int k,int target,vector<vector<int>>&dp){
         if(target == 0 and n==0)
             return 1;
@@ -8,8 +10,9 @@ public:
             return 0;
         if(dp[n][target]!=-1)
             return dp[n][target];
-        
-        int ways=0,MOD=1000000007;
+//         (a+b)%c = (a%c + b%c)%c
+//         (ways+tempAns)%MOD
+        int ways=0,MOD=1e9+7;
         for(int i=1;i<=k;++i){
             int tempAns = totalWays(n-1,k,target - i, dp)%MOD;
             ways = ways % MOD;
