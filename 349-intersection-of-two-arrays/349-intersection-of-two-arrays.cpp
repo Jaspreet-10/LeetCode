f6ea78d0e4071bec34a2bb3ab9750f32fc080610
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-05-05 23:27:18 +0530

    Time: 16 ms (15.47%), Space: 10.5 MB (40.72%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -3,13 +3,13 @@ public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int,int>m;
         vector<int>v;
-        for(int i=0;i<nums1.size();++i){
+        for(int i = 0 ; i < nums1.size() ; ++i){
             m[nums1[i]]++;
         }
-        for(int i=0;i<nums2.size();++i){
-            if(m[nums2[i]] and m[nums2[i]]!=-1){
+        for(int i = 0 ; i < nums2.size() ; ++i){
+            if(m[nums2[i]]>0){
                 v.push_back(nums2[i]);
-                m[nums2[i]]=-1;
+                m[nums2[i]] = -1;
             }
         }
         return v;
