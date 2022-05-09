739b24f4cccdb0f92b62418a79182218b94eb3a0
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-05-09 15:25:02 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,22 +10,22 @@ using namespace std;
 
 class Solution{
   public:
-    int maxProfit(int index,int price[],int n,map<pair<int,int>,int>&memo){
+    int maxProfit(int index,int price[],int n,vector<vector<int>>&dp){
         if(index>=n || n==0)
             return 0;
-        pair<int,int>currentKey={index,n};
-        if(memo.find(currentKey)!=memo.end())
-            return memo[currentKey];
+        if(dp[index][n]!=-1)
+            return dp[index][n];
         int consider=0;
         if(index+1<=n)
-        consider=price[index]+maxProfit(index,price,n-(index+1),memo);
-        int notConsider=maxProfit(index+1,price,n,memo);
-        memo[currentKey]=max(consider,notConsider);
-        return memo[currentKey];
+        consider=price[index]+maxProfit(index,price,n-(index+1),dp);
+        int notConsider=maxProfit(index+1,price,n,dp);
+        dp[index][n]=max(consider,notConsider);
+        return dp[index][n];
     }
     int cutRod(int price[], int n) {
         map<pair<int,int>,int>memo;
-        return maxProfit(0,price,n,memo);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return maxProfit(0,price,n,dp);
     }
 };
 
