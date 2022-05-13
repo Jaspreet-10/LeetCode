c597401c4524e47f2e484693e7d6160a8d75aacb
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-05-13 12:30:05 +0530

    Time: 38 ms (57.39%), Space: 8.5 MB (30.05%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -12,7 +12,9 @@ public:
             return dp[n][target];
 //         (a+b)%c = (a%c + b%c)%c
 //         (ways+tempAns)%MOD
-        int ways=0,MOD=1e9+7;//How the values in ways is getting store. Didn't get this.
+        int ways=0,MOD=1e9+7;//We are intializing way = 0 beacuse from every node we need to get 
+//                             different ways, like count works for every duplicate elements in the
+//                              two for loops. we need to intialize count =0 in first loop again and again for diffrent elemnets, this is how ways is working ony recursion is taking care of thw ways here
         for(int i=1;i<=k;++i){
             int tempAns = totalWays(n-1,k,target - i, dp)%MOD;
             ways = ways % MOD;
