efe2b2cb7bd1b18fddba7af7d3ed4801372585f0
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-05-24 00:24:08 +0530

    Time: 3 ms (91.91%), Space: 9.7 MB (72.99%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        auto it=find(nums.begin(),nums.end(),target);
-        if(it!=nums.end()){
-            return (it-nums.begin());
-        }
-        if(target<nums[0]){
+        if(target<nums[0])
             return 0;
-        }
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]<target and nums[i+1]>target){
+        for(int i = 0 ; i < nums.size()-1 ; ++i){
+            if(nums[i]==target)
+                return i;
+            if(target>nums[i] and target<nums[i+1])
                 return i+1;
-            }
         }
+        if(nums[nums.size()-1]==target)
+            return nums.size()-1;
         return nums.size();
     }
 };
