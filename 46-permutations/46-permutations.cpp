6e4de0343dc1fed93dcef65f175dc4af3415c8bb
Jaspreet-10 <64219292+Jaspreet-10@users.noreply.github.com>
2022-06-23 17:13:43 +0530

    Time: 15 ms (6.67%), Space: 8.8 MB (9.65%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
+    void printPermutations(vector<int>&nums,vector<vector<int>>&ans,vector<int>ds,vector<bool>freq)     {
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i = 0 ; i < nums.size() ; ++i){
+            if(!freq[i]){
+                ds.push_back(nums[i]);
+                freq[i]=true;
+                printPermutations(nums,ans,ds,freq);
+                freq[i]=false;
+                ds.pop_back();
+            }
+        }
+        return;
+    }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> v;
-        sort(nums.begin(),nums.end());
-        int i=0;
-        do{
-                v.push_back(nums);
-        }while(next_permutation(nums.begin(),nums.end()));
-        return v;
+        vector<vector<int>>ans;
+        vector<int>ds;
+        vector<bool>freq(nums.size(),false);
+        printPermutations(nums,ans,ds,freq);
+        return ans;
     }
 };
