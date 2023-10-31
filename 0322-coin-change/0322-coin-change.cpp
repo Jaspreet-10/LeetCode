a7ee980b078d0664d11ee7861aa5dccd76bf2727
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2023-10-31 12:49:05 -0700

    Time: 125 ms (12.86%), Space: 50.5 MB (9.67%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-     // int ans=INT_MAX;
-     int totalWays(int amount,vector<int>& coins,int index,int n, vector<vector<int>>&dp){
-        if(amount==0)
+    int findMinimumCoins(int index, vector<int>&coins, int amount, vector<vector<int>>&dp){
+        if(amount == 0){
             return 0;
-        if(index>=n)
-            return 10001;
-        // string currentKey=to_string(index)+"-"+to_string(amount);
-        // if(memo.find(currentKey)!=memo.end())
-        //     return memo[currentKey];
-         if(dp[index][amount]!=-1)
-             return dp[index][amount];
-        int consider=10001;
-        if(coins[index]<=amount)
-            consider=1+totalWays(amount-coins[index],coins,index,n,dp);
-        int notConsider=totalWays(amount,coins,index+1,n,dp);
-        dp[index][amount]=min(consider,notConsider);
+        }
+        if(index>=coins.size() or amount<0) return 10001;
+        
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        int consider = 10001;
+        if(amount>=coins[index])
+        consider = 1 + findMinimumCoins(index, coins, amount-coins[index],dp);
+        int notConsider = findMinimumCoins(index+1, coins, amount,dp);
+        dp[index][amount] = min(consider,notConsider);
         return dp[index][amount];
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
-        // memset(dp,-1,sizeof(dp));//Time Taken is O(n).
-        int ans= totalWays(amount,coins,0,coins.size(),dp);
-        if(ans==10001)
-            return -1;
-        return ans;
+       vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
+           int ans = findMinimumCoins(0, coins, amount, dp);
+           if(ans == 10001) return -1;
+           return ans;
     }
 };
