5d5507dad545fd09170e2f17133cd29b5b156379
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-06-27 16:38:24 -0700

    Time: 52 ms (22.78%), Space: 47.9 MB (17.8%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int findMinimumCoins(int index, vector<int>&coins, int amount, vector<vector<int>>&dp){
-        if(amount == 0){
-            return 0;
-        }
-        if(index>=coins.size() or amount<0) return 10001;
-        
-        if(dp[index][amount]!=-1) return dp[index][amount];
-        int consider = 10001;
-        if(amount>=coins[index])
-        consider = 1 + findMinimumCoins(index, coins, amount-coins[index],dp);
-        int notConsider = findMinimumCoins(index+1, coins, amount,dp);
-        dp[index][amount] = min(consider,notConsider);
-        return dp[index][amount];
+    int minCoins(int i, int n, vector<int>&coins, int amount, vector<vector<int>>&dp){
+        if(amount == 0) return 0;
+        if(amount<0 || i >= n) return 10001;
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int consider = 1 + minCoins(i, n, coins, amount-coins[i], dp);
+        int notConsider = minCoins(i+1, n, coins, amount, dp);
+        dp[i][amount] = min(consider, notConsider);
+        return dp[i][amount];
     }
     int coinChange(vector<int>& coins, int amount) {
-       vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
-           int ans = findMinimumCoins(0, coins, amount, dp);
-           if(ans == 10001) return -1;
-           return ans;
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans = minCoins(0, n, coins, amount, dp);
+        if(ans == 10001) return -1;
+        return ans;
     }
 };
