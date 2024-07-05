3666fdf5266541327bd075bb8d313c8514859f5b
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-07-05 02:01:47 -0700

    Time: 76 ms (5.33%), Space: 28 MB (11.23%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findSubRoot(TreeNode* root, TreeNode* subRoot){
+        if(!root and !subRoot) return true;
+        if(!root or !subRoot) return false;
+        if(root->val == subRoot->val){
+            cout<<root->val<<" ";
+            if(!findSubRoot(root->left, subRoot->left)) return false;
+            if(!findSubRoot(root->right, subRoot->right)) return false;
+        }else return false;
+        return true;
+    }
+    bool dfs(TreeNode* root, TreeNode* subRoot){
+        if(!root) return false;
+        if(root->val == subRoot->val){
+            if(findSubRoot(root, subRoot)) return true;
+        }
+        if(dfs(root->left, subRoot) || dfs(root->right,subRoot)) return true;
+        return false;
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!root or !subRoot) return false;
+        return dfs(root, subRoot);
+    }
+};
