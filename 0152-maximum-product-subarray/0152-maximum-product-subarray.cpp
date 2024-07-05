dfc98dbd7400b5ca60cd02a7e7542b93a5b10079
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-07-05 16:01:57 -0700

    Time: 8 ms (23.27%), Space: 16.5 MB (5.37%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -6,10 +6,11 @@ public:
     double maxi1 = -DBL_MAX, maxi2 = -DBL_MAX;
 
     while (left < nums.size() && right >= 0) {
-        maxi2 = std::max(maxi2, std::max((double)nums[left], (double)nums[right]));
-        maxi1 = std::max(maxi1, std::max(leftSum * nums[left], rightSum * nums[right]));
+       
         leftSum = nums[left] * leftSum;
         rightSum = nums[right] * rightSum;
+        maxi1 = max(maxi1, max(leftSum, rightSum));
+        maxi2 = max(maxi2, max((double)nums[left], (double)nums[right]));
         // Reset if the product is zero
         if (leftSum == 0) leftSum = 1;
         if (rightSum == 0) rightSum = 1;
