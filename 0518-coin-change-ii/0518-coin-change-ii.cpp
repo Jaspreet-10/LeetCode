f747715365fdb2ec7db1fec83759ff8a79b2d255
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-07-05 19:34:31 -0700

    Time: 19 ms (22.4%), Space: 20.1 MB (22.27%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+     int totalWays(int index, int n, vector<int>&coins, int amount, vector<vector<int>>&dp){
+        if(amount == 0) return 1;
+        if(index>=n || amount<0) return 0;
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        int consider = totalWays(index,n,coins,amount-coins[index],dp);
+        int notConsider = totalWays(index+1,n,coins,amount,dp);
+        return dp[index][amount] = (consider+notConsider);
+    }
+    int change(int amount, vector<int>& coins) {
+         int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans = totalWays(0,n,coins,amount,dp);
+        if(ans == 10001) return 0;
+        return ans;
+    }
+};
