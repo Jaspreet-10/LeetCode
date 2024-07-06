2f8eebc79976b114240f0b41cd87c998e6e26c62
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-07-05 23:30:19 -0700

    Time: 2 ms (44.14%), Space: 10.1 MB (99.03%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size()-1, c = 0, i = n;
+        if(digits[n]+1>=10){
+            c = 1;
+            while(i>=0){
+                int sum = digits[i]+c;
+                int rem = sum%10;
+                c = sum/10;
+                digits[i] = rem;
+                --i;
+            }
+            if(c>=1) digits.insert(digits.begin(),1);
+        }else{
+            digits[n]+=1;
+        }
+        return digits;
+    }
+};
