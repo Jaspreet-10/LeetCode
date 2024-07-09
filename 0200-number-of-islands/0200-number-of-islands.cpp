8486156205ef10f0082969ada28c1ce361db3a32
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-07-08 20:44:51 -0700

    Time: 27 ms (70.55%), Space: 15.7 MB (81.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,7 +3,7 @@ public:
     void countIslands(int i, int j, vector<vector<char>>& grid){
         if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0' 
         || grid[i][j] == '.') return ;
-        grid[i][j] = '.';
+        grid[i][j] = '0';
         countIslands(i+1,j,grid);
         countIslands(i,j+1,grid);
         countIslands(i-1,j,grid);
