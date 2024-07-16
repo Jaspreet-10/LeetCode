4bd27eda4eb8b58f2a299f2535022ce6715efe4c
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-07-15 18:52:24 -0700

    Time: 0 ms (100%), Space: 7.9 MB (20.93%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i = 0 ; i < s.size() ; ++i){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{') st.push(s[i]);
+            else if(s[i] == ')' and !st.empty() and st.top() == '('){
+                st.pop();
+                continue;
+            }
+            else if(s[i] == ']' and !st.empty() and st.top() == '['){
+                st.pop();
+                continue;
+            }
+            else if(s[i] == '}' and !st.empty() and st.top() == '{') {
+                st.pop();
+                continue;
+            }
+            else return false;
+        }
+        if(st.empty()) return true;
+        return false;
+    }
+};
