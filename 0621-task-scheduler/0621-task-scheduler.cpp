3d1235fb8fa54ee44fbda0efed630844ddf2d73d
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-07-19 18:37:19 -0700

    Time: 71 ms (52.06%), Space: 41.9 MB (42.42%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int>ch(26,0);
-        for(int i = 0 ; i < tasks.size() ; ++i) ch[tasks[i] - 'A']++;
-        sort(ch.rbegin(), ch.rend());
-        int maxVal = ch[0]-1;
-        int idle_slots = maxVal*n;
-        for(int i = 1 ; i < 26 ; ++i){
-            idle_slots-=min(ch[i],maxVal);
+        priority_queue<int>pq;
+        queue<pair<int,int>>q;
+        vector<int>charCount(26,0);
+        int intervals = 0;
+        for(char ch : tasks){
+            // cout<<ch-'A'<<" ";
+            charCount[ch-'A']++;
         }
-        return idle_slots>0 ? idle_slots+tasks.size():tasks.size();
+        for(int i = 0 ; i < 26 ; ++i){
+            if(charCount[i]>0)
+            pq.push(charCount[i]);
+        }
+        while(!pq.empty() or !q.empty()){
+            intervals+=1;
+            if(!pq.empty()){
+            int val = pq.top();
+            pq.pop();
+            if(val>1)
+            q.push({val-1,intervals+n});
+            }
+            if(!q.empty() && q.front().second == intervals){ 
+                pq.push(q.front().first);
+                q.pop();
+            }
+        }
+        return intervals;
     }
 };
