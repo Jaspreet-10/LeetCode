f2b9f9ea35935ac072d89590f2a664fca97b94a0
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-07-24 18:54:57 -0700

    Time: 3 ms (33.97%), Space: 11 MB (5.91%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int numberOfWays(int i, int j, int m, int n, vector<vector<int>>&dp){
-        if(i == m-1 and j == n-1) return 1;
-        if(i>=m || j>=n || i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        dp[i][j] = numberOfWays(i+1,j,m,n,dp) + numberOfWays(i,j+1,m,n,dp);
-        return dp[i][j];
+    int findUniquePaths(int cr, int cc, int row, int col, map<pair<int,int>,int>&dp){
+        if(cr==row-1 && cc==col-1) return 1;
+        if(cr<0 || cc<0 || cr>=row || cc>=col) return 0;
+        if(dp[{cr,cc}]>0) return dp[{cr,cc}];
+        int down = findUniquePaths(cr+1,cc,row,col,dp);
+        int right = findUniquePaths(cr,cc+1,row,col,dp);
+        dp[{cr,cc}] = down+right;
+        return down+right;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return numberOfWays(0,0,m,n,dp);
+        map<pair<int,int>,int>dp;
+        return findUniquePaths(0,0,m,n,dp);
     }
 };
