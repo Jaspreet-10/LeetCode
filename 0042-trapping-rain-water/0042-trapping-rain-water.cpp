4aabf7e99d02be256851657a34649c3c969a5985
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-04 14:33:17 -0700

    Time: 6 ms (89.3%), Space: 23.5 MB (18.47%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    int trap(vector<int>& arr) {
-        int n = arr.size();
-        int left[n],right[n], maxi = INT_MIN;
-        int trappedWater = 0;
-        
+    int trap(vector<int>& height) {
+        int n = height.size(), trappedWater = 0;
+        vector<int>leftArray(n,0), rightArray(n,0);
+        int maxLeft = 0, maxRight = n-1;
         for(int i = 0 ; i < n ; ++i){
-            left[i] = max(arr[i],maxi);
-            maxi = left[i];
+            if(height[i]>=height[maxLeft]){
+                leftArray[i] = i;
+            }
+            else{
+                leftArray[i] = maxLeft;
+            }
+            maxLeft = leftArray[i];
         }
-        maxi = INT_MIN;
-        
-        for(int i = n-1 ; i >= 0 ; --i){
-            right[i] = max(arr[i],maxi);
-            maxi = right[i];
+        for(int i = n-1 ; i >=0 ; --i){
+            if(height[i]>=height[maxRight]){
+                rightArray[i] = i;
+            }
+            else{
+                rightArray[i] = maxRight;
+            }
+            maxRight = rightArray[i];
         }
         for(int i = 0 ; i < n ; ++i){
-            trappedWater+=min(left[i],right[i])-arr[i];
+            trappedWater+=((min(height[leftArray[i]], height[rightArray[i]]) - height[i]));
         }
         return trappedWater;
     }
