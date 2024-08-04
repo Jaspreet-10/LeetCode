67769605d06814a6ada0a174e87a49272b3db73d
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-04 16:34:14 -0700

    Time: 541 ms (5.2%), Space: 274 MB (5.05%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int maxProfit = 0, buy = INT_MAX, sell = 0;
-        for(int i = 0 ; i < prices.size() ; ++i){
-            if(prices[i]<buy){
-                buy = prices[i];
-            }
-            if(prices[i]>buy and prices[i]-buy>maxProfit){
-                maxProfit = prices[i]-buy;
-            }
+    int bestTrans(vector<int>&prices,int curr,bool canbuy , int trans,vector<vector<vector<int>>>&vec)
+    {
+        
+        if(curr>=prices.size()) return 0;
+        if(trans<=0) return 0;
+        if(vec[curr][canbuy][trans]!= -1)
+           return vec[curr][canbuy][trans];
+        if(canbuy)
+        {
+            int idle = bestTrans(prices,curr+1,canbuy,trans,vec); 
+            int buy = -prices[curr]+bestTrans(prices,curr+1,!canbuy,trans,vec);
+            return vec[curr][canbuy][trans]=max(idle,buy);
         }
-        return maxProfit;
+        else{
+            int idle1 = bestTrans(prices,curr+1,canbuy,trans,vec);
+            int sell = prices[curr]+bestTrans(prices,curr+1,canbuy,trans-1,vec);
+            return vec[curr][canbuy][trans]=max(idle1,sell);
+        }
+        
+    }
+    int maxProfit(vector<int>& prices) {
+       vector<vector<vector<int>>> vec(prices.size(),vector<vector<int>>(2,vector<int>(2,-1)));
+        return bestTrans(prices,0,1,1,vec);
     }
 };
