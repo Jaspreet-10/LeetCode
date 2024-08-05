b99134a037579c761c0199859562f5e51b7f22de
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-05 00:44:33 -0700

    Time: 7 ms (63.52%), Space: 8.8 MB (56.07%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s1.length()>s2.length())
-            return false;
-        vector<int>arr1(26,0),arr2(26,0);
-        int right=0,left=0,size=s2.length();
-        for(int i=0;i<s1.length();++i){
-            arr1[s1[i]-'a']+=1;
-            arr2[s2[i]-'a']+=1;
-            right++;
+        vector<int>charArray1(26,0), charArray2(26,0);
+        for(int i = 0 ; i < s1.size() ; ++i) charArray1[s1[i] - 'a']++;
+        int i = 0, j = 0;
+        while(j<s2.size()){
+            charArray2[s2[j]-'a']++;
+            if(charArray2 == charArray1) return true;
+            while((j-i+1)>=s1.size()){
+                charArray2[s2[i]-'a']--;
+                ++i;
+            }
+            ++j;
         }
-        while(right<size){
-            if(arr1==arr2) return true;
-            if(right<size)
-                arr2[s2[right]-'a']+=1;
-                arr2[s2[left]-'a']-=1;
-            left+=1;
-            right+=1;
-        }
-        if(arr1 == arr2) return true;
         return false;
     }
 };
