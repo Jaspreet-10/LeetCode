373ef230f3bb7e5c4e7a32558713b0c99b238b80
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-04 17:18:21 -0700

    Time: 13 ms (58.29%), Space: 11.9 MB (49.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int answer = 0, j = 0;
-        unordered_map<char,int>m;
-        for(int i = 0 ; i < s.length() ; ++i){
-                while(j<i and m[s[i]]>0){
-                    m[s[j]]--;
-                    ++j;
-                }
-            m[s[i]]++;
-            answer = max(answer,i-j+1);
+        int left = 0, right = 0, maxi = 0;
+        unordered_map<char, int>m;
+        while(right < s.size()){
+            m[s[right]]++;
+            while(m[s[right]]>1){
+                m[s[left]]--;
+                ++left;
+            }
+            maxi = max(maxi, right - left + 1);
+            ++right;
         }
-        return answer;
+        return maxi;
     }
 };
-
-// Combining these observations:
-
-// The outer loop runs 
-// O(n) times.
-// The inner while loop runs 
-// O(n) times in total over the entire string.
-// Therefore, the overall time complexity is 
-// O(n).
