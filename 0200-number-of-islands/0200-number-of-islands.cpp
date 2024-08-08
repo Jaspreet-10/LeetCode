7b066eec50db6f28831f7f86dd11e61c7554bbc6
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-08 11:28:31 -0700

    Time: 30 ms (48.83%), Space: 17 MB (48.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    void countIslands(int i, int j, vector<vector<char>>& grid){
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == '0' 
-        || grid[i][j] == '.') return ;
-        grid[i][j] = '0';
-        countIslands(i+1,j,grid);
-        countIslands(i,j+1,grid);
-        countIslands(i-1,j,grid);
-        countIslands(i,j-1,grid);
+    void countIslands(int row, int column, vector<vector<char>>&grid, vector<vector<int>>&vis, 
+    int numberOfRowsInTheGrid, int numberOfColumnInTheGrid){
+        if(row<0 || column<0 || row>=numberOfRowsInTheGrid || 
+        column>=numberOfColumnInTheGrid || grid[row][column] == '0' || vis[row][column] == -1)
+            return ;
+        vis[row][column] = -1;
+        countIslands(row-1,column,grid,vis,numberOfRowsInTheGrid,numberOfColumnInTheGrid);
+        countIslands(row+1,column,grid,vis,numberOfRowsInTheGrid,numberOfColumnInTheGrid);
+        countIslands(row,column-1,grid,vis,numberOfRowsInTheGrid,numberOfColumnInTheGrid);
+        countIslands(row,column+1,grid,vis,numberOfRowsInTheGrid,numberOfColumnInTheGrid);
         return ;
     }
     int numIslands(vector<vector<char>>& grid) {
-        int cnt = 0;
-        for(int i = 0 ; i < grid.size() ; ++i){
-            for(int j = 0 ; j < grid[0].size() ; ++j){
-                if(grid[i][j] == '1'){
-                    ++cnt;
-                    countIslands(i,j,grid);
+        int numberOfRowsInTheGrid = grid.size(),
+        numberOfColumnsInTheGrid = grid[0].size(); 
+        int counter = 0;
+        vector<vector<int>>vis(numberOfRowsInTheGrid,vector<int>(numberOfColumnsInTheGrid,0));
+        for(int i = 0 ; i < numberOfRowsInTheGrid ; ++i){
+            for(int j = 0 ; j < numberOfColumnsInTheGrid ; ++j){
+                if(grid[i][j] == '1' && vis[i][j]!=-1){
+                    counter+=1;
+                    countIslands(i,j,grid,vis, numberOfRowsInTheGrid, numberOfColumnsInTheGrid);
                 }
             }
         }
-        return cnt;
+         return counter;
     }
 };
