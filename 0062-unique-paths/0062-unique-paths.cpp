25ed16f3229561ea599cae0bcd303d55aea5322f
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-09 21:16:55 -0700

    Time: 3 ms (34.17%), Space: 8 MB (11.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int findUniquePaths(int cr, int cc, int row, int col, map<pair<int,int>,int>&dp){
+    int findUniquePaths(int cr, int cc, int row, int col, vector<vector<int>>&dp){
         if(cr==row-1 && cc==col-1) return 1;
         if(cr<0 || cc<0 || cr>=row || cc>=col) return 0;
-        if(dp[{cr,cc}]>0) return dp[{cr,cc}];
+        if(dp[cr][cc]!=-1) return dp[cr][cc];
         int down = findUniquePaths(cr+1,cc,row,col,dp);
         int right = findUniquePaths(cr,cc+1,row,col,dp);
-        dp[{cr,cc}] = down+right;
-        return down+right;
+        return dp[cr][cc] = down+right;
     }
     int uniquePaths(int m, int n) {
-        map<pair<int,int>,int>dp;
+        vector<vector<int>>dp(m,vector<int>(n,-1));
         return findUniquePaths(0,0,m,n,dp);
     }
 };
