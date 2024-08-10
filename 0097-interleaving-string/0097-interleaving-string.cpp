23e248e10987332283927f8c5ba00397d783c9a2
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-10 12:35:42 -0700

    Time: 33 ms (21.3%), Space: 36 MB (20.14%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -4,24 +4,22 @@ public:
    int i1, int i2, int i3, map<pair<pair<int,int>,int>,bool>&m) {
     // Base cases
     if (i3 == len3) return (i1 == len1 && i2 == len2);
-    bool ans = false;
+    bool ans1 = false, ans2 = false;
     if(m.find({{i1,i2},i3})!=m.end()) return m[{{i1,i2},i3}];
     if (i1 < len1 && s1[i1] == s3[i3]) {
-        ans = checkIsInterleave(s1, s2, s3, len1, len2, len3, i1 + 1, i2, i3 + 1,m);
+        ans1 = checkIsInterleave(s1, s2, s3, len1, len2, len3, i1 + 1, i2, i3 + 1,m);
     }
     if (i2 < len2 && s2[i2] == s3[i3]) {
-        ans = ans || checkIsInterleave(s1, s2, s3, len1, len2, len3, i1, i2 + 1, i3 + 1,m);
+        ans2 = checkIsInterleave(s1, s2, s3, len1, len2, len3, i1, i2 + 1, i3 + 1,m);
     }
-    m[{{i1,i2},i3}] = ans;
-    return ans;
+    return m[{{i1,i2},i3}] = ans1 || ans2;
 }
     bool isInterleave(string s1, string s2, string s3) {
-        int len1 = s1.size();
+    int len1 = s1.size();
     int len2 = s2.size();
     int len3 = s3.size();
     map<pair<pair<int,int>,int>,bool>m;
     if (len1 + len2 != len3) return false;
-
     return checkIsInterleave(s1, s2, s3, len1, len2, len3, 0, 0, 0,m);
     }
 };
