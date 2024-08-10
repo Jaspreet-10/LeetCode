d030caf8ff3f88e2bffbb6799671b980102804ef
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-10 12:10:00 -0700

    Time: 4 ms (57.4%), Space: 15 MB (9.93%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int bestTrans(vector<int>&prices,int curr,bool canbuy,vector<vector<int>>&vec)
-    {
-        
-        if(curr>=prices.size()) return 0;
-        if(vec[curr][canbuy]!=-1) return vec[curr][canbuy];
-        if(canbuy)
-        {
-            int idle = bestTrans(prices,curr+1,canbuy,vec); 
-            int buy = -prices[curr]+bestTrans(prices,curr+1,false,vec);
-            return vec[curr][canbuy]=max(idle,buy);
+    int trans(int index, int n, vector<int>&prices, vector<vector<int>>&dp, bool canBuy){
+        if(index >= n) return 0;
+        if(dp[index][canBuy]!=-1) return dp[index][canBuy];
+        if(canBuy){
+            int idle = trans(index+1,n,prices,dp,canBuy);
+            int profit = -prices[index] + trans(index+1,n,prices,dp,false);
+             dp[index][canBuy] = max(idle, profit);
+        }else{
+            int idle = trans(index+1,n,prices,dp,canBuy);
+            int profit = prices[index] + trans(index+2,n,prices,dp,true);
+            dp[index][canBuy] = max(idle, profit);
         }
-        else{
-            int idle1 = bestTrans(prices,curr+1,canbuy,vec);
-            int sell = prices[curr]+bestTrans(prices,curr+2,true,vec);// Cool Down Period
-            return vec[curr][canbuy]=max(idle1,sell);
-        }
-        
+        return dp[index][canBuy];
     }
     int maxProfit(vector<int>& prices) {
-      vector<vector<int>>vec(prices.size(),vector<int>(2,-1));
-        return bestTrans(prices,0,1,vec);
+        int n = prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return trans(0,n,prices,dp,true);
     }
 };
