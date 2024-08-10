667423da0e7012ae05f53c43d1cbae98a4b22ab6
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-10 15:10:36 -0700

    Time: 4 ms (36.88%), Space: 9.6 MB (13.02%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.cpp
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -3,12 +3,13 @@ public:
     int lastStoneWeight(vector<int>& stones) {
         priority_queue<int>pq;
         for(int i = 0 ; i < stones.size() ; ++i) pq.push(stones[i]);
-        while(pq.size()>1){
-            int t1 = pq.top();pq.pop();
-            int t2 = pq.top();pq.pop();
-            if(t1 != t2) pq.push(abs(t1-t2));
+        while(pq.size()>=2){
+            int y = pq.top();pq.pop();
+            int x = pq.top();pq.pop();
+            if(y == x) continue;
+            else pq.push(y-x);
         }
-        if(pq.empty()) return 0;
+        if(pq.size() == 0) return 0;
         return pq.top();
     }
 };
