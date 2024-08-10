45e5d59906bafa1547fde1ff1ede50916cb3990f
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-10 14:55:07 -0700

    Time: 31 ms (22.21%), Space: 24.8 MB (38.07%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -1,13 +1,12 @@
 class KthLargest {
 public:
-    priority_queue<int, vector<int>, greater<int>>pq;
     int k;
-    vector<int>temp;
+    priority_queue<int,vector<int>,greater<int>>pq;
     KthLargest(int k, vector<int>& nums) {
         this->k = k;
         for(int i = 0 ; i < nums.size() ; ++i){
             pq.push(nums[i]);
-            if(pq.size()>k) pq.pop();
+            if(pq.size()>k) pq.pop(); 
         }
     }
     
