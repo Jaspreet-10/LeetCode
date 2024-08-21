8a68bfd47d31659d6df53d6966626417bf5e9c79
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-21 14:20:22 -0700

    Time: 3 ms (83.88%), Space: 16.6 MB (19.52%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int V = grid.size(), maxi = INT_MIN, ans = 0;
-        vector<vector<bool>>vis(grid.size(),vector<bool>(grid[0].size(),false));
+        if(grid.size() == 0) return -1;
+        int m = grid.size(), n = grid[0].size();
+        int countOranges = 0;
+        vector<vector<int>>vis(m,vector<int>(n,0));
         queue<pair<pair<int,int>,int>>q;
-        for(int i = 0 ; i < grid.size() ; ++i){
-            for(int j = 0 ; j < grid[i].size() ; ++j){
+        for(int i = 0 ; i < m ; ++i){
+            for(int j = 0 ; j < n ; ++j){
                 if(grid[i][j] == 2){
-                    vis[i][j] = true;
-                    q.push({{i,j},0});
+                     grid[i][j] = -1;
+                     q.push({{i,j},0});
                 }
             }
         }
         while(!q.empty()){
-            pair<pair<int,int>,int>head = q.front();
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            countOranges = q.front().second;
             q.pop();
-            int row[4] = {-1,0,+1,0};
-            int col[4] = {0,-1,0,+1};
+            int r[4] = {-1, 0, 1, 0};
+            int c[4] = {0, -1, 0, 1};
             for(int i = 0 ; i < 4 ; ++i){
-                int newRow = row[i]+head.first.first;
-                int newCol = col[i]+head.first.second;
-                if(newRow>=0 && newCol>=0 && newRow<grid.size() &&
-                newCol < grid[0].size() and grid[newRow][newCol] == 1
-                && !vis[newRow][newCol]){
-                    ans = head.second+1;
-                    vis[newRow][newCol] = true;
-                    q.push({{newRow,newCol},head.second+1});
+                int newRow = r[i]+row;
+                int newCol = c[i]+col;
+                if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && 
+                grid[newRow][newCol] == 1){
+                     grid[newRow][newCol] = -1;
+                    q.push({{newRow,newCol},countOranges+1});
                 }
             }
         }
-        for(int i = 0 ; i < grid.size() ; ++i){
-            for(int j = 0 ; j < grid[i].size() ; ++j){
-                if(grid[i][j] == 1 and vis[i][j] == false){
-                    return -1;
-                }
+        for(int i = 0 ; i < m ; ++i){
+            for(int j = 0 ; j < n ; ++j){
+                if(grid[i][j] == 1) return -1;
             }
         }
-        return ans;
+        return countOranges;
     }
 };
