3377b23d0bf10e9d198b7b069c2b433469faf055
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-22 12:26:49 -0700

    Time: 0 ms (100%), Space: 8.5 MB (78.04%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-
-void totalSubsets(vector<int>& nums, vector<int>& v, int index) {
-    // Base case: if the index is greater than or equal to the size of nums, push the current subset
-    if (index >= nums.size()) {
-        res.push_back(v);
-        return;
+    void findSubsets(int index, vector<int>&nums, vector<vector<int>>&ans, int n,
+        unordered_map<int,int>&m, vector<int>&temp){
+            if(index>=n){
+                ans.push_back(temp);
+                return ;
+            }
+            temp.push_back(nums[index]);
+            findSubsets(index+1, nums, ans, n, m, temp);
+            temp.pop_back();
+            findSubsets(index+1, nums, ans, n, m, temp);
+            return;
+        }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        unordered_map<int, int>m;
+        vector<int>temp;
+        findSubsets(0, nums, ans,  nums.size(), m, temp);
+        return ans;
     }
-    
-    // Recursive case 1: Do not take the current index element
-    totalSubsets(nums, v, index + 1);
-    
-    // Recursive case 2: Take the current index element
-    v.push_back(nums[index]);
-    totalSubsets(nums, v, index + 1);
-    
-    // Backtrack by removing the last element
-    v.pop_back();
-}
-
-vector<vector<int>> subsets(vector<int>& nums) {
-    vector<int> v;
-    res.clear(); // Clear the result vector before starting
-    totalSubsets(nums, v, 0);
-    return res;
-}
 };
