4c91c41af92cb9d99b7ce5b2fbdeeb8eb8149be6
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-22 13:09:45 -0700

    Time: 275 ms (5.53%), Space: 126.3 MB (5.12%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -4,21 +4,24 @@ public:
     vector<vector<int>>powerSet(vector<int>& nums, vector<vector<int>>&ans,
     int index, vector<int>v){
         if(index>=nums.size()){
-            sort(v.begin(),v.end());
-            if(m[v] == 0)
+            // sort(v.begin(),v.end());
+            // if(m[v] == 0)
             ans.push_back(v);
-            m[v]++;
+            // m[v]++;
             return ans;
         }
         v.push_back(nums[index]);
         powerSet(nums,ans,index+1,v);
         v.pop_back();
+        while (index != nums.size() - 1 && nums[index] == nums[index + 1])
+            index++;
         powerSet(nums,ans,index+1,v);
         return ans;
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int>v;
         vector<vector<int>>ans;
+        sort(nums.begin(), nums.end());
         return powerSet(nums,ans,0,v);
         // return ans;
     }
