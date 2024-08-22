58a15a2181c3273f557c1bf2e9ceb28276232f89
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-22 12:43:14 -0700

    Time: 0 ms (100%), Space: 12.6 MB (54.89%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     void findCombinations(int index, int n, vector<int>&candidates, vector<vector<int>>&ans,
      int target, vector<int>&temp){
-        if(index>=n || target<0) return ;
+        if(index>=n) return ;
         if(target == 0){
            ans.push_back(temp);
            return ;
         }
+        if(candidates[index]<=target){
         temp.push_back(candidates[index]);
         findCombinations(index, n, candidates, ans, target-candidates[index], temp);
         temp.pop_back();
+        }
         findCombinations(index+1, n, candidates, ans, target, temp);
         return ;
     }
