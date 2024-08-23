dd51aabdbe6812ddd932beb5978ed148a685c257
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-23 13:13:36 -0700

    Time: 14 ms (26.54%), Space: 9.7 MB (28.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int i = 0, j = 0, maxi = 0, max_count = 0;
+        int max_count = 0, j = 0, i = 0, ans = 0;
         unordered_map<char,int>m;
         while(i<s.length()){
             m[s[i]]++;
             max_count = max(max_count, m[s[i]]);
-            while(i - j - max_count+1 > k){
+            while(j<i && i - j - max_count+1 > k){
                 m[s[j]]--;
+                max_count = max(max_count, m[s[j]]);
                 ++j;
             }
-            maxi = max(maxi, i-j+1);
+            ans = max(ans, i - j + 1);
             ++i;
         }
-        return maxi;
+        return ans;
     }
 };
