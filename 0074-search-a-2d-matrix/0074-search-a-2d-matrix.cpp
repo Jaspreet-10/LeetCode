aaa2ac410522ed41c9e5458ec64dd012d0530949
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-24 10:52:59 -0700

    Time: 7 ms (22.33%), Space: 12.3 MB (31.44%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
+    bool findElement(vector<vector<int>>&matrix, int target, int row){
+        int low = 0, high = matrix[row].size()-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(matrix[row][mid] == target) return true;
+            else if(matrix[row][mid]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return false;
+    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int findRow = 0, col = matrix[0].size();
         for(int i = 0 ; i < matrix.size() ; ++i){
             if(matrix[i][col-1]>=target){
-                findRow = i;
-                break;
+                return findElement(matrix,target,i);
             }
-        }
-        for(int i = 0 ; i < matrix[findRow].size() ; ++i){
-            if(matrix[findRow][i] == target) return true;
         }
         return false;
     }
