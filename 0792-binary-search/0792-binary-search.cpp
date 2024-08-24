c74ff0d95282762acbc0cb8477b607c5f9655f61
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-24 10:46:08 -0700

    Time: 25 ms (67.64%), Space: 30.2 MB (18.35%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int binarySearch(int low, int high, vector<int>&nums, int target){
+    int findElement(vector<int>&nums, int target){
+        int low = 0, high = nums.size()-1;
         while(low<=high){
             int mid = (low+high)/2;
             if(nums[mid] == target) return mid;
-            if(nums[mid] > target){
-                high  = mid-1;
-            }else low = mid+1;
+            else if(nums[mid]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
         }
         return -1;
     }
     int search(vector<int>& nums, int target) {
-        int high = nums.size()-1;
-        return binarySearch(0,high,nums,target);
+        return findElement(nums,target);
     }
 };
