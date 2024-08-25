b70fb4841c64f3f45cc27f1098995a54df5499b3
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-25 16:22:20 -0700

    Time: 276 ms (29.69%), Space: 62 MB (10.45%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
      int totalWays(int index, int n, vector<int>&nums, int target,map<pair<int,int>,int>&m){
         if(index==n and target == 0) return 1;
-        if(index==n and target!=0) return 0;
+        if(index>=n and target!=0) return 0;
         pair<int,int>p = {index,target};
         if(m.find(p)!=m.end()) return m[p];
         int consider = totalWays(index+1,n,nums,target-nums[index],m);
