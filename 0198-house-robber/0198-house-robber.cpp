fcbf74308544f2ceeab4df0ef8e14d2d69132edf
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-25 12:39:39 -0700

    Time: 0 ms (100%), Space: 9.7 MB (10.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int maximumProfit(int index, int n, vector<int>&nums,vector<int>&dp){
-        if(index >= n) return 0;
+    int robberHelper(int index, vector<int>&nums, int n, vector<int>&dp){
+        if(index>=n) return 0;
         if(dp[index]!=-1) return dp[index];
-        int consider = nums[index]+maximumProfit(index+2,n,nums,dp);
-        int notConsider = maximumProfit(index+1,n,nums,dp);
-        dp[index] = max(consider,notConsider);
-        return dp[index];
+        int consider = nums[index] + robberHelper(index+2, nums, n, dp);
+        int notConsider = robberHelper(index+1, nums, n, dp);
+        return dp[index] = max(consider, notConsider);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int>dp(n,-1);
-        return maximumProfit(0,n,nums,dp);
+        return robberHelper(0, nums, n, dp);
     }
 };
