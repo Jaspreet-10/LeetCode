b0ca9cef6aa7335246493fd9875e4ad33d4c93e3
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-25 13:28:52 -0700

    Time: 6 ms (10.05%), Space: 11.5 MB (10.23%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        vector<int> dp(s.length() + 1, -1);
-        return codes(s, dp, 0);
+    int decodeWays(int index, string s,  vector<int>&dp){
+        if(index>=s.size()) return 1;
+        if(dp[index]!=-1) return dp[index];
+        int oneStep = 0, twoStep = 0;
+        if(s[index]!='0'){
+        oneStep = decodeWays(index+1, s, dp);
+        if(index+1 < s.length() && stoi(s.substr(index,2)) <= 26) 
+            twoStep = decodeWays(index+2, s, dp);
+        }
+        return dp[index] = oneStep + twoStep;
     }
-    
-    int codes(string s, vector<int>& dp, int index) {
-        if (index >= s.length()) {
-            return 1;
-        }
-        
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-        
-        int count = 0;
-        
-        if (s[index] != '0') {
-            count += codes(s, dp, index + 1);
-            if (index + 1 < s.length() && stoi(s.substr(index, 2)) <= 26) {
-                count += codes(s, dp, index + 2);
-            }
-        }
-        dp[index] = count;
-        return count;
+    int numDecodings(string s) {
+        vector<int>dp(s.size(),-1);
+        return decodeWays(0, s, dp);
     }
 };
