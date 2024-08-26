f3d1ce0685072774997a1466a0a6c8ff50bdd978
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-26 12:42:45 -0700

    Time: 2 ms (43.96%), Space: 10.3 MB (76.95%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -4,7 +4,7 @@ public:
         int n = digits.size()-1, c = 0, i = n;
         if(digits[n]+1>=10){
             c = 1;
-            while(i>=0){
+            while(i>=0 && c>0){
                 int sum = digits[i]+c;
                 int rem = sum%10;
                 c = sum/10;
