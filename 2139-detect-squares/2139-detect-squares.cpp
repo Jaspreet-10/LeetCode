f47ccb4ccbef48c2b1193d2942e272496a87da45
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-26 13:39:21 -0700

    Time: 201 ms (74.63%), Space: 99.3 MB (77.74%) - LeetHub

--- 2139-detect-squares/2139-detect-squares.cpp
+++ 2139-detect-squares/2139-detect-squares.cpp
@@ -2,6 +2,7 @@ class DetectSquares {
 public:
     map<pair<int,int>,int>m;
     DetectSquares() {
+        
     }
     
     void add(vector<int> point) {
@@ -9,19 +10,14 @@ public:
     }
     
     int count(vector<int> point) {
-         int ans = 0;
-        for (auto it : m) {
-            int x = it.first.first, y = it.first.second;
-            int px = point[0], py = point[1];
-
-            // Check if we have the same x or y coordinate
-            if (abs(x - px) != abs(y - py) || x == px || y == py) {
-                continue;
-            }
-            
-            // Check if the other two points of the square exist
-            if (m.find({x, py}) != m.end() && m.find({px, y}) != m.end()) {
-                ans += m[{x, py}] * m[{px, y}] * it.second;
+        int ans = 0;
+        int px = point[0], py = point[1];
+        for(auto it : m){
+            int x = it.first.first;
+            int y = it.first.second;
+            if(abs(px-x)!=abs(py-y) || x == px || y == py) continue;
+            if(m.find({px,y})!=m.end() && m.find({x,py})!=m.end()){
+                ans+=m[{x,py}]*m[{px,y}]*it.second;
             }
         }
         return ans;
