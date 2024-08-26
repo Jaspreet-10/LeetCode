cbed1d2821c9d1c191489c29a7008497d6c17fa2
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-25 17:57:00 -0700

    Time: 17 ms (19.14%), Space: 21 MB (18.13%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,17 +4,17 @@ public:
         vector<vector<int>>ans;
         intervals.push_back(newInterval);
         sort(intervals.begin(), intervals.end());
-        vector<int>temp = {intervals[0][0], intervals[0][1]};
-        for(int i = 1 ; i < intervals.size() ; ++i){
-           if(intervals[i][0]<=temp[1]){
-                temp[1] = max(temp[1], intervals[i][1]);
-           }else{
-                ans.push_back(temp);
-                temp[0] = intervals[i][0];
-                temp[1] = intervals[i][1];
-           }
+        int a = intervals[0][0], b = intervals[0][1];
+        for(int i = 0 ; i < intervals.size() ; ++i){
+            if(intervals[i][0]<=b){
+                b = max(intervals[i][1],b);
+            }else{
+                ans.push_back({a,b});
+                a = intervals[i][0];
+                b = intervals[i][1];
+            }
         }
-        ans.push_back(temp);
+        ans.push_back({a,b});
         return ans;
     }
 };
