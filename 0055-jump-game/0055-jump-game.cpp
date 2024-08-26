c377463f605eb92b64bea19b3c86807b6e0f4dcc
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-26 11:42:17 -0700

    Time: 41 ms (72%), Space: 51.1 MB (37.46%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,7 +4,7 @@ public:
         int reachable = 0;
         for(int i = 0 ; i < nums.size() ; ++i){
             if(i>reachable) return false;
-            reachable = max(reachable, nums[i]+i);
+            reachable = max(reachable, i+nums[i]);
         }
         return true;
     }
