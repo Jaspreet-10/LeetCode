f48688fdd37cb687bf5a654510f101afae83bde6
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-08-27 12:13:52 -0700

    Time: 1021 ms (23.46%), Space: 11.8 MB (44.16%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     bool searchWord(int index, int cr, int cc, int m, int n, vector<vector<char>>& board, 
-    string word, char temp){
+    string word){
         if(word.size() == index) return true;
-        if(index>=word.size() || cr<0 || cc<0 || cr>=m ||cc >=n || 
-        board[cr][cc] == '.' || board[cr][cc]!=word[index]) return false;
+        if(index>=word.size() || cr<0 || cc<0 || cr>=m ||cc >=n || board[cr][cc] == '.') 
+            return false;
         if(word[index] == board[cr][cc]){
-            temp = board[cr][cc];
+            char temp = board[cr][cc];
             board[cr][cc] = '.';
-            if(searchWord(index+1, cr+1, cc, m, n, board, word, temp)) return true;
-            if(searchWord(index+1, cr-1, cc, m, n, board, word, temp)) return true;
-            if(searchWord(index+1, cr, cc+1, m, n, board, word, temp)) return true;
-            if(searchWord(index+1, cr, cc-1, m, n, board, word, temp)) return true;
+            if(searchWord(index+1, cr+1, cc, m, n, board, word)) return true;
+            if(searchWord(index+1, cr-1, cc, m, n, board, word)) return true;
+            if(searchWord(index+1, cr, cc+1, m, n, board, word)) return true;
+            if(searchWord(index+1, cr, cc-1, m, n, board, word)) return true;
             board[cr][cc] = temp;
         }
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        char temp;
         for(int i = 0 ; i < board.size() ; ++i){
             for(int j = 0 ; j < board[i].size() ; ++j){
-                if(searchWord(0, i, j, board.size(), board[0].size(), board, word, temp))
+                if(searchWord(0, i, j, board.size(), board[0].size(), board, word))
                     return true;
             }
         }
