6ad2a68a89f9fe5697349a0c035bd9e198c16a68
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-13 10:46:10 -0800

    Time: 0 ms (100%), Space: 9.4 MB (17.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-    transform(s.begin(), s.end(), s.begin(), ::tolower);
-    string str = "";
-    int c = 0;
-    for(int i = 0 ; i < s.length() ; ++i){
-        int ch = s[i];
-        if((ch>=48&& ch<=57) || (ch>=97 && ch<=122)){
-            c = -1;
-            str+=s[i];
+        string str = "";
+        transform(s.begin(), s.end(), s.begin(), ::tolower); 
+        for(int i = 0 ; i < s.length() ; ++i){
+            int ch1 = s[i];
+            if((ch1>=97 && ch1<=122) || (ch1>=48 && ch1<=57)){
+                str+=s[i];
             }
         }
-        cout<<str<<" ";
-        if(c == 0)
-            return true;
         int i = 0, j = str.length()-1;
-        while(i<=j){
-            if(str[i]!=str[j])
-                return false;
+        while(i<j){
+            if(str[i]!=str[j]) return false;
             ++i,--j;
         }
         return true;
