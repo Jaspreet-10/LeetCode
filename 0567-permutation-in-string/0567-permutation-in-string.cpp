92f43aac475112e140a4dfa02645ec46e610395f
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-16 07:42:21 -0800

    Time: 0 ms (100%), Space: 8.8 MB (48.46%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int>charArray1(26,0), charArray2(26,0);
-        for(int i = 0 ; i < s1.size() ; ++i) charArray1[s1[i] - 'a']++;
+        vector<int>v1(26,0),v2(26,0);
         int i = 0, j = 0;
-        while(j<s2.size()){
-            charArray2[s2[j]-'a']++;
-            if(charArray2 == charArray1) return true;
-            while((j-i+1)>=s1.size()){
-                charArray2[s2[i]-'a']--;
-                ++i;
+        for(int i = 0 ; i < s1.length() ; ++i) v1[s1[i]-'a']++;
+        while(i<s2.length()){
+            while(i-j>=s1.length()){
+                v2[s2[j]-'a']--;
+                ++j;
             }
-            ++j;
+            // cout<<s2.substr(j,i-j+1)<<"\n";
+            v2[s2[i]-'a']++;
+            if(v1 == v2) return true;
+            ++i;
         }
         return false;
     }
