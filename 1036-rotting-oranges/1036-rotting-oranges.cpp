4b016a71271305b950700f316fd178e84f8f8195
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-17 15:28:33 -0800

    Time: 2 ms (38.46%), Space: 16.6 MB (24.59%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.size() == 0) return -1;
+    int bfs(vector<vector<int>>& grid, queue<pair<int,pair<int,int>>>&q, int&time){
         int m = grid.size(), n = grid[0].size();
-        int countOranges = 0;
-        vector<vector<int>>vis(m,vector<int>(n,0));
-        queue<pair<pair<int,int>,int>>q;
-        for(int i = 0 ; i < m ; ++i){
-            for(int j = 0 ; j < n ; ++j){
-                if(grid[i][j] == 2){
-                     grid[i][j] = -1;
-                     q.push({{i,j},0});
-                }
-            }
-        }
         while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
-            countOranges = q.front().second;
+            time = q.front().first;
+            pair<int,int>p = q.front().second;
             q.pop();
-            int r[4] = {-1, 0, 1, 0};
+            int row = p.first, col = p.second;
+             int r[4] = {-1, 0, 1, 0};
             int c[4] = {0, -1, 0, 1};
-            for(int i = 0 ; i < 4 ; ++i){
+           for(int i = 0 ; i < 4 ; ++i){
                 int newRow = r[i]+row;
                 int newCol = c[i]+col;
                 if(newRow>=0 && newCol>=0 && newRow<m && newCol<n && 
                 grid[newRow][newCol] == 1){
                      grid[newRow][newCol] = -1;
-                    q.push({{newRow,newCol},countOranges+1});
+                    q.push({time+1, {newRow,newCol}});
                 }
             }
         }
-        for(int i = 0 ; i < m ; ++i){
-            for(int j = 0 ; j < n ; ++j){
-                if(grid[i][j] == 1) return -1;
+        return time;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<int,pair<int,int>>>q;
+        int time = 0;
+        vector<vector<int>>vis(grid.size(), vector<int>(grid[0].size(),-1));
+        for(int i = 0 ; i < grid.size() ; ++i){
+            for(int j = 0 ; j < grid[i].size() ; ++j){
+                if(grid[i][j] == 2){
+                    grid[i][j] = -1;
+                    q.push({0,{i,j}});
+                }
             }
         }
-        return countOranges;
+       time = bfs(grid,q,time);
+       for(int i = 0 ; i < grid.size() ; ++i){
+        for(int j = 0 ; j < grid[i].size() ; ++j){
+            if(grid[i][j] == 1) return -1;
+        }
+       }
+        return time;
     }
 };
