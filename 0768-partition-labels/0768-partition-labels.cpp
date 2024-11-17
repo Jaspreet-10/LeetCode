e2fa2c285e73f0b0dd0f5f8b698a577b257b6487
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-16 21:36:08 -0800

    Time: 0 ms (100%), Space: 9.2 MB (56.51%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         unordered_map<char,int>m;
-        string str = "";
         vector<int>v;
-        for(int i = s.size()-1 ; i >= 0 ; --i){
+        int j = 0, count = 0;
+        for(int i = s.size()-1;i>=0;--i){
            if(m[s[i]] == 0)
-            m[s[i]] = i+1; 
+            m[s[i]] = i+1;
         }
-        int maxi = m[s[0]];
-        for(int i = 0 ; i < s.size() ; ++i){
-            if(m[s[i]]>maxi) maxi = m[s[i]];
-            str+=s[i];
-            if(maxi-1 == i){ 
-            maxi = INT_MIN;
-            v.push_back(str.length());
-            str = "";
-            }
+        int index = m[s[0]];
+        while(j<s.size()){
+            index = max(index,m[s[j]]); 
+            ++count;
+            if(index-1 == j){
+                index = INT_MIN;
+                v.push_back(count);
+                count = 0;
+            }  
+            ++j;
         }
         return v;
     }
