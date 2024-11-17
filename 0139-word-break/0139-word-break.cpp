323b2c450f2dd4e576a6620a9243eba64879caf9
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-17 11:10:22 -0800

    Time: 7 ms (56.63%), Space: 13.2 MB (56.71%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    bool wordBreakHelper(int index, string s, unordered_set<string>&st, vector<int>&dp){
-        if(index >= s.size()) return true;
+    bool helper(int index, string s, set<string>&st, vector<int>&dp){
+        if(index>=s.size()) return true;
         string str = "";
         if(dp[index]!=-1) return dp[index];
         for(int i = index ; i < s.size() ; ++i){
             str+=s[i];
             if(st.find(str)!=st.end()){
-                if(wordBreakHelper(i+1, s, st,dp)) return dp[index] = true;
+                if(helper(i+1, s, st, dp)) return dp[index] = true;
             }
         }
         return dp[index] = false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string>st(wordDict.begin(), wordDict.end());
-        string str = "";
-        vector<int>dp(s.size(),-1);
-        return wordBreakHelper(0, s, st,dp);
+        vector<int>dp(s.size(), -1);
+        set<string>st(wordDict.begin(), wordDict.end());
+        return helper(0, s, st, dp);
     }
 };
