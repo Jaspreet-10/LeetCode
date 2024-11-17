7cac504733c455015e648599d90a759d41f055ec
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-17 10:59:30 -0800

    Time: 0 ms (100%), Space: 16.4 MB (93.51%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-    double leftSum = 1, rightSum = 1;
-    int left = 0, right = nums.size() - 1;
-    double maxi1 = -DBL_MAX, maxi2 = -DBL_MAX;
-
-    while (left < nums.size() && right >= 0) {
-       
-        leftSum = nums[left] * leftSum;
-        rightSum = nums[right] * rightSum;
-        maxi1 = max(maxi1, max(leftSum, rightSum));
-        maxi2 = max(maxi2, max((double)nums[left], (double)nums[right]));
-        // Reset if the product is zero
-        if (leftSum == 0) leftSum = 1;
-        if (rightSum == 0) rightSum = 1;
-        
-        ++left;
-        --right;
-    }
-    cout<<maxi2<<" "<<maxi1<<" ";
-    cout<<(int)(std::max(maxi1, maxi2));
-    return (int)(std::max(maxi1, maxi2));
+        long long start = 1, end = 1, maxi = INT_MIN;
+        int i = 0, j = nums.size()-1;
+        while(i<nums.size() && j>=0){
+            start*=nums[i];
+            end*=nums[j];
+            maxi = max(max(start,end), maxi);
+            if(start == 0) start = 1;
+            if(end == 0) end = 1;
+            ++i, --j;
+        }
+        return maxi;
     }
 };
