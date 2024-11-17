74203499fb3ab479ef7d83a7687219ad262dbdd6
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-17 10:40:46 -0800

    Time: 134 ms (34.55%), Space: 26.8 MB (38.13%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.length();
-        int start = 0, maxLen = 1;
+        int n = s.size();
+        int start = 0, maxLen = 0;
         vector<vector<bool>>dp(n, vector<bool>(n,false));
         for(int i = 0 ; i < n ; ++i){
             dp[i][i] = true;
+            maxLen = 1;
         }
-        for(int i = 0 ; i < n-1 ; ++i){
-            if(s[i] == s[i+1]){ 
+        for(int i = 0 ; i < n ; ++i){
+            if(s[i] == s[i+1]){
                 dp[i][i+1] = true;
                 start = i;
                 maxLen = 2;
             }
         }
         for(int i = 2 ; i < n ; ++i){
-            for(int j = 0 ; j < n-i ; ++j){
-                if(s[j] == s[j+i] && dp[j+1][j+i-1]){
-                    dp[j][j+i] = true;
+            for(int j = 0 ; j< n-i ; ++j){
+                if(s[j] == s[i+j] && dp[j+1][j+i-1]){
                     start = j;
                     maxLen = i+1;
+                    dp[j][j+i] = true;
                 }
             }
         }
-        return maxLen == 0 ?  "" : s.substr(start, maxLen);
+        return (maxLen>=1)?s.substr(start, maxLen):"";
     }
 };
