ace28e56efb83e6bb6dec8940963662d9576f728
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-17 09:47:54 -0800

    Time: 483 ms (5.04%), Space: 10.6 MB (31.12%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        if (num1 == "0" || num2 == "0") return "0";
-        
-        vector<int> res(num1.size()+num2.size(), 0);
-        
-        for (int i = num1.size()-1; i >= 0; i--) {
-            for (int j = num2.size()-1; j >= 0; j--) {
-                res[i + j + 1] += (num1[i]-'0') * (num2[j]-'0');
-                res[i + j] += res[i + j + 1] / 10;
-                res[i + j + 1] %= 10;
+        if(num1 == "0" || num2 == "0") return "0";
+        int n1 = num1.size(), n2 = num2.size();
+        vector<int>res(n1 + n2, 0);
+        for(int i = n1-1;i>=0;--i){
+            for(int j = n2-1;j>=0;--j){
+                cout<<(num1[i]-'0') * (num2[j]-'0')<<" ";
+                res[i+j+1]+=(num1[i]-'0') * (num2[j]-'0');
+                res[i+j]+= res[i+j+1]/10;
+                res[i+j+1]%=10;
             }
         }
-        
         int i = 0;
-        string ans = "";
-        while (res[i] == 0) i++;
-        while (i < res.size()) ans += to_string(res[i++]);
-        
-        return ans;
+        string str = "";
+        while(res[i] == 0)++i;
+        for(int j = i ; j < res.size() ; ++j){
+            str+=to_string(res[j]);
+        }
+        return str;
     }
 };
