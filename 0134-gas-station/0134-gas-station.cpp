b103e9a34c8ecb8ba5f6ff71f61327dafe81653e
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-16 20:42:39 -0800

    Time: 3 ms (39.52%), Space: 111.3 MB (17.56%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int n=gas.size();
-        int total_gas=0,total_cost=0;
-        int curr_gas=0, starting_point=0;
-        for(int i=0;i<n;i++)
-        {
-            //these two variable are to check if no case is possible
+        int total_gas = 0, cur_gas = 0, total_cost = 0, index = 0;
+        for(int i = 0 ; i < gas.size() ; ++i){
             total_gas+=gas[i];
             total_cost+=cost[i];
-            //for checking the total present gas at index i
-            curr_gas+=gas[i]-cost[i];
-            if(curr_gas<0)
-            {
-               //there is a breakdown....so we will start from next point or index
-                starting_point=i+1;
-                //reset our fuel 
-                curr_gas=0;
+            cur_gas+=gas[i] - cost[i];
+            if(cur_gas<0){
+                index = i+1;
+                cur_gas = 0;
             }
         }
-        return (total_gas<total_cost)?-1:starting_point;     
+        return (total_gas<total_cost)?-1:index;
     }
 };
