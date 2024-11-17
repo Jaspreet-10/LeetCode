9805a9cbd720cbe8f5da0f2a31f031a3af915884
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-17 10:51:01 -0800

    Time: 2824 ms (5.02%), Space: 362.5 MB (5%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int minCoins(int i, int n, vector<int>&coins, int amount, vector<vector<int>>&dp){
+     int climbStairsHelper(int index, int n, map<pair<int,int>,int>&m, vector<int>&coins,
+     int amount){
         if(amount == 0) return 0;
-        if(amount<0 || i >= n) return 10001;
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        int consider = 1 + minCoins(i, n, coins, amount-coins[i], dp);
-        int notConsider = minCoins(i+1, n, coins, amount, dp);
-        dp[i][amount] = min(consider, notConsider);
-        return dp[i][amount];
+        if(index >= n || amount<0) return 10001;
+        if(m[{index, amount}]>0) return m[{index,amount}];
+        int consider = 1 + climbStairsHelper(index,n,m,coins,amount-coins[index]);
+        int notConsider = climbStairsHelper(index+1,n,m,coins,amount);
+        return m[{index, amount}] = min(consider,notConsider);
     }
     int coinChange(vector<int>& coins, int amount) {
+        map<pair<int,int>,int>m;
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = minCoins(0, n, coins, amount, dp);
+        int ans = climbStairsHelper(0,n,m,coins,amount);
         if(ans == 10001) return -1;
         return ans;
     }
