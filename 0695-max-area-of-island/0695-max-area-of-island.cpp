eb231e8149d7c563a876697db856f39239b6216c
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2024-11-17 15:02:33 -0800

    Time: 0 ms (100%), Space: 26.9 MB (82.58%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    int countIslands(int i, int j, vector<vector<int>>& grid, int& cnt){
-        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0 
-        || grid[i][j] == -1) return 0;
-        grid[i][j] = -1;
+    int dfs(int i, int j, int m, int n, vector<vector<int>>& grid, int &cnt){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j] == 0) return cnt;
+        grid[i][j] = 0;
         ++cnt;
-        countIslands(i+1,j,grid,cnt);
-        countIslands(i,j+1,grid,cnt);
-        countIslands(i-1,j,grid,cnt);
-        countIslands(i,j-1,grid,cnt);
+        dfs(i+1,j,m,n,grid,cnt);
+        dfs(i-1,j,m,n,grid,cnt);
+        dfs(i,j+1,m,n,grid,cnt);
+        dfs(i,j-1,m,n,grid,cnt);
         return cnt;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-         int cnt = 0,ans = 0;
-        for(int i = 0 ; i < grid.size() ; ++i){
-            for(int j = 0 ; j < grid[0].size() ; ++j){
+          int r = grid.size(), c = grid[0].size();
+        int cnt = 0, maxi = 0;
+        for(int i = 0 ; i < r ; ++i){
+            for(int j = 0 ; j < c ; ++j){
                 if(grid[i][j] == 1){
-                    int cnt = 0;
-                    countIslands(i,j,grid,cnt);
-                    ans = max(ans,cnt);
+                maxi = max(dfs(i,j,r,c,grid,cnt), maxi);
+                cnt = 0;
                 }
             }
         }
-        return ans;
+        return maxi;
     }
 };
