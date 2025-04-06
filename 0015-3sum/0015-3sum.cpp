f2bbe6967ba428e4fbdae2ecb6495a9d34868bb0
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-06 16:41:35 -0700

    Time: 39 ms (95.85%), Space: 29 MB (87.82%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>>res;
         sort(nums.begin(), nums.end());
+        vector<vector<int>>v;
         for(int i = 0 ; i < nums.size() ; ++i){
-            if(i>0 and nums[i] == nums[i-1]) continue;
-            int first = nums[i];
             int j = i+1, k = nums.size()-1;
             while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
+                int sum = nums[i] + nums[j] + nums[k];
                 if(sum == 0){
-                    res.push_back({nums[i],nums[j],nums[k]});
-                    ++j,--k;
-                    while(j<k and nums[j] == nums[j-1]) ++j;
-                    while(j<k and nums[k] == nums[k+1]) --k;
+                v.push_back({nums[i], nums[j], nums[k]});
+                ++j, --k;
+                while(j<nums.size() && nums[j] == nums[j-1]) ++j;
+                while(k>=0 && nums[k] == nums[k+1]) --k;;
                 }
-                else if(sum>0) --k;
+                else if(sum>0)--k;
                 else ++j;
             }
+            while(i<nums.size()-1 && nums[i] == nums[i+1]) ++i;
         }
-        return res;
+        return v;
     }
 };
