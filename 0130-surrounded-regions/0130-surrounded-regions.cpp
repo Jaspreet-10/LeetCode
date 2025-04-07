a41ad96160aea5eb5a8363a3b54b891ae5511b54
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-07 09:49:53 -0700

    Time: 0 ms (100%), Space: 13.9 MB (90.65%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    void helper(int i, int j, int m, int n, vector<vector<char>>&board,
-                vector<vector<int>>&vis){
-        if(i<0 || j<0 || j>=n || i>=m || board[i][j]=='X' || vis[i][j]==0) return;
-        board[i][j] = 'O';
-        vis[i][j] = 0;
-        helper(i+1,j,m,n,board,vis);
-        helper(i-1,j,m,n,board,vis);
-        helper(i,j+1,m,n,board,vis);
-        helper(i,j-1,m,n,board,vis);
+    void dfs(int i, int j, int m, int n, vector<vector<char>>&board){
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j] == 'X' || board[i][j] == '1') return;
+        board[i][j] = '1';
+        int rowC[4] = {0, -1, 0, 1};
+        int colC[4] = {-1, 0, 1, 0};
+        for(int k = 0 ; k < 4 ; ++k){
+            dfs(rowC[k] + i, colC[k] + j, m, n, board);
+        }
         return ;
     }
     void solve(vector<vector<char>>& board) {
         int m = board.size(), n = board[0].size();
-        vector<vector<int>>vis(m,vector<int>(n,-1));
         for(int i = 0 ; i < m ; ++i){
             for(int j = 0 ; j < n ; ++j){
-                if((i == 0 || j == 0 || j == n-1 || i == m-1) && board[i][j] == 'O'
-                and vis[i][j]==-1){
-                    helper(i,j,m,n,board,vis);
+                if((i == 0 || j == 0 || i == m-1 || j == n-1) && board[i][j] == 'O'){
+                    dfs(i,j,m,n,board);
                 }
             }
         }
-         for(int i = 0 ; i < m ; ++i){
+        for(int i = 0 ; i < m ; ++i){
             for(int j = 0 ; j < n ; ++j){
-                if(board[i][j] == 'O' and vis[i][j]==-1){
+                if(board[i][j] == '1'){
+                    board[i][j] = 'O';
+                }
+                else if(board[i][j] == 'O'){
                     board[i][j] = 'X';
                 }
             }
