a87f839ec866ca22d2dd289622df3cc34fb9033b
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-10 13:04:14 -0700

    Time: 19 ms (51.79%), Space: 43 MB (46.08%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int>m;
         priority_queue<int>pq;
         queue<pair<int,int>>q;
-        int total_time = 0;
-        vector<int>v(26,0);
-        for(int i = 0 ; i < tasks.size() ; ++i) v[tasks[i]-'A']++;
-        for(int i = 0 ; i < v.size() ; ++i){
-            if(v[i]>0) pq.push(v[i]);
+        int time = 0;
+        for(int i = 0 ; i < tasks.size() ; ++i){
+            m[tasks[i]]++;
+        }
+        for(auto it : m){
+            pq.push(it.second);
         }
         while(!pq.empty() or !q.empty()){
-            total_time+=1;
-           if(!pq.empty()){
-            int val = pq.top();
+            time+=1;
+            if(!pq.empty()){
+            int element = pq.top();
+            element-=1;
             pq.pop();
-            if(val>1)
-            q.push({val-1,total_time+n});
+            if(element>0){
+                q.push({element, time+n});
             }
-            if(!q.empty() && q.front().second == total_time){
+            }
+            if(q.size() > 0 and q.front().second<=time){ 
                 pq.push(q.front().first);
                 q.pop();
             }
         }
-        return total_time;
+        return time;
     }
 };
