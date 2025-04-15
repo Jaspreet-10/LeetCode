47b34e22542bec54d0b59f455c5f50a3ed97f28a
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-15 10:22:49 -0700

    Time: 64 ms (11.73%), Space: 49.7 MB (23.24%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,7 +3,7 @@ public:
     int helper(int index, vector<int>&coins, int n, int amount, vector<vector<int>>&dp){
         if(amount == 0) return 0;
         if(amount<0 || index>=n) return 10001;
-        if(dp[index][amount]!=10001) return dp[index][amount];
+        if(dp[index][amount]!=-1) return dp[index][amount];
 
         int consider = 1 + helper(index, coins, n, amount-coins[index], dp);
         int notConsider = helper(index+1, coins, n, amount, dp);
@@ -12,7 +12,7 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1, 10001));
+        vector<vector<int>>dp(n, vector<int>(amount+1, -1));
         int ans = helper(0, coins, n, amount, dp);
         if(ans == 10001) return -1;
         return ans;
