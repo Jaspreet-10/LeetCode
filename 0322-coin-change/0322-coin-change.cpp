d98c9cd1c6ef1e6b0903da4c1e90a6bf21381fc1
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-15 10:19:19 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int minCoins(int i, int n, vector<int>&coins, int amount, vector<vector<int>>&dp){
+    int helper(int index, vector<int>&coins, int n, int amount, vector<vector<int>>&dp){
         if(amount == 0) return 0;
-        if(amount<0 || i >= n) return 10001;
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        int consider = 1 + minCoins(i, n, coins, amount-coins[i], dp);
-        int notConsider = minCoins(i+1, n, coins, amount, dp);
-        dp[i][amount] = min(consider, notConsider);
-        return dp[i][amount];
+        if(amount<0 || index>=n) return 10001;
+        if(dp[index][amount]!=10001) return dp[index][amount];
+
+        int consider = 1 + helper(index, coins, n, amount-coins[index], dp);
+        int notConsider = helper(index+1, coins, n, amount, dp);
+        
+        return dp[index][amount] = min(consider, notConsider);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans = minCoins(0, n, coins, amount, dp);
+        vector<vector<int>>dp(n, vector<int>(amount+1, 10001));
+        int ans = helper(0, coins, n, amount, dp);
         if(ans == 10001) return -1;
         return ans;
     }
