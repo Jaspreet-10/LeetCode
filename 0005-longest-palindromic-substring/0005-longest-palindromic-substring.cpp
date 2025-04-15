b7592e0f9d08123fce58a8d297733cf848bf077b
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-14 19:29:19 -0700

    Time: 95 ms (39.33%), Space: 25.9 MB (43.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -11,19 +11,19 @@ public:
         for(int i = 0 ; i < n ; ++i){
             if(s[i] == s[i+1]){
                 dp[i][i+1] = true;
-                start = i;
                 maxLen = 2;
+                start = i;
             }
         }
         for(int i = 2 ; i < n ; ++i){
-            for(int j = 0 ; j< n-i ; ++j){
+            for(int j = 0; j < n-i ; ++j){
                 if(s[j] == s[i+j] && dp[j+1][j+i-1]){
+                    dp[j][j+i] = true;
                     start = j;
                     maxLen = i+1;
-                    dp[j][j+i] = true;
                 }
             }
         }
-        return (maxLen>=1)?s.substr(start, maxLen):"";
+        return (maxLen >= 1) ? s.substr(start, maxLen) : "";
     }
 };
