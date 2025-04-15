85aa59fee372ee520bd952d64bcf03904ef208eb
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-15 14:12:33 -0700

    Time: 0 ms (100%), Space: 9.4 MB (33.09%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int findWays(int i, int j, int m, int n, vector<vector<int>>&dp){
+    int findUniquePaths(int i, int j, int m, int n, vector<vector<int>>&dp){
+        if(i < 0 || j < 0 || i>=m || j>=n) return 0;
         if(i == m-1 && j == n-1) return 1;
-        if(i>=m || j>=n) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int down = findWays(i+1, j, m, n, dp);
-        int right = findWays(i, j+1, m, n, dp);
-        return dp[i][j] = down+right;
+        int down = findUniquePaths(i+1, j, m, n, dp);
+        int right = findUniquePaths(i, j+1, m, n, dp);
+        return dp[i][j] = right+down;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m, vector<int>(n,-1));
-        return findWays(0,0,m,n,dp);
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        return findUniquePaths(0, 0, m, n, dp);
     }
 };
