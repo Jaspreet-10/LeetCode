c33a2609f975ac2138a6ccf7353bd9c89782005c
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-15 14:34:20 -0700

    Time: 49 ms (8.3%), Space: 27.7 MB (14.75%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int lcs(int i, int j, int n, int m, string &text1, string &text2, 
-    map<pair<int,int>,int>&M){
-        if(i>=n || j>=m) return 0;
-        pair<int,int>p = {i,j};
-        if(M.find(p)!=M.end()) return M[p];
-
+    int checkLCS(int i, int j, int m, int n, string &text1, string &text2, 
+    vector<vector<int>>&dp){
+        if(i>=m || j>=n) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
         int consider = 0, notConsider = 0;
-        if(text1[i] == text2[j]) 
-        consider = 1 + lcs(i+1, j+1, n, m, text1, text2, M);
-        else{
-            notConsider = max(lcs(i+1, j, n, m, text1, text2, M),
-            lcs(i, j+1, n, m, text1, text2, M));
+        if(text1[i] == text2[j]){
+           consider = 1+checkLCS(i+1, j+1, m, n, text1, text2, dp);
+        }else{
+            notConsider = max(checkLCS(i, j+1, m, n, text1, text2, dp), 
+                            checkLCS(i+1, j, m, n, text1, text2, dp));
         }
-       return M[p] = max(consider,notConsider);
+        return dp[i][j] = max(consider, notConsider);
+
     }
     int longestCommonSubsequence(string text1, string text2) {
-        map<pair<int,int>,int>m;
-        return lcs(0, 0, text1.length(), text2.length(), text1, text2, m);
+        int m = text1.size(), n = text2.size();
+        vector<vector<int>>dp(m+1, vector<int>(n+1,-1));
+        return checkLCS(0, 0, m, n, text1, text2, dp);
     }
 };
