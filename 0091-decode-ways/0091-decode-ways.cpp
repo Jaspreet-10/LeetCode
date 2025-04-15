95302fe1d485beed51f56b867794fff0059cb51c
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-15 09:35:51 -0700

    Time: 0 ms (100%), Space: 11.2 MB (8.81%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int decodeWays(int index, string s,  vector<int>&dp){
-        if(index>=s.size()) return 1;
+    int decodeWays(int index, int n, string s,  vector<int>&dp){
+        if(index>=n) return 1;
         if(dp[index]!=-1) return dp[index];
-        int oneStep = 0, twoStep = 0;
+        int c1 = 0, c2 = 0;
         if(s[index]!='0'){
-        oneStep = decodeWays(index+1, s, dp);
-        if(index+1 < s.length() && stoi(s.substr(index,2)) <= 26) 
-            twoStep = decodeWays(index+2, s, dp);
+            c1 = decodeWays(index+1, n, s, dp);
+        if(index+1 < n && stoi(s.substr(index, 2))<=26){
+            c2 = decodeWays(index+2, n, s, dp);
         }
-        return dp[index] = oneStep + twoStep;
+        }
+        return dp[index] = c1+c2;
     }
     int numDecodings(string s) {
-        vector<int>dp(s.size(),-1);
-        return decodeWays(0, s, dp);
+        int n = s.size();
+        vector<int>dp(n,-1);
+        return decodeWays(0, n, s, dp);
     }
 };
