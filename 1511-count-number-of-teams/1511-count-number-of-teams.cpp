4a89cfcb99a66cfd9646a6c690e92be2907ce68a
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-16 00:17:27 -0700

    Time: 16 ms (75.97%), Space: 12.5 MB (84.66%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    int n;
-
-    int countIncreasing(int i, int count, int prev, vector<int>& rating, vector<vector<vector<int>>>& dpInc) {
-        if (count == 3) return 1;
-        if (i == n) return 0;
-
-        if (dpInc[i][count][prev + 1] != -1) return dpInc[i][count][prev + 1];
-
-        int pick = 0;
-        if (prev == -1 || rating[i] > rating[prev]) {
-            pick = countIncreasing(i + 1, count + 1, i, rating, dpInc);
-        }
-        int skip = countIncreasing(i + 1, count, prev, rating, dpInc);
-
-        return dpInc[i][count][prev + 1] = pick + skip;
-    }
-
-    int countDecreasing(int i, int count, int prev, vector<int>& rating, vector<vector<vector<int>>>& dpDec) {
-        if (count == 3) return 1;
-        if (i == n) return 0;
-
-        if (dpDec[i][count][prev + 1] != -1) return dpDec[i][count][prev + 1];
-
-        int pick = 0;
-        if (prev == -1 || rating[i] < rating[prev]) {
-            pick = countDecreasing(i + 1, count + 1, i, rating, dpDec);
-        }
-        int skip = countDecreasing(i + 1, count, prev, rating, dpDec);
-
-        return dpDec[i][count][prev + 1] = pick + skip;
-    }
-
     int numTeams(vector<int>& rating) {
-        n = rating.size();
-        vector<vector<vector<int>>> dpInc(n, vector<vector<int>>(4, vector<int>(n + 1, -1)));
-        vector<vector<vector<int>>> dpDec(n, vector<vector<int>>(4, vector<int>(n + 1, -1)));
+        int n = rating.size();
+        int count = 0;
 
-        return countIncreasing(0, 0, -1, rating, dpInc) + countDecreasing(0, 0, -1, rating, dpDec);
+        for (int j = 1; j < n - 1; ++j) {
+            int leftSmaller = 0, leftGreater = 0;
+            int rightSmaller = 0, rightGreater = 0;
+
+            // Count elements on the left of j
+            for (int i = 0; i < j; ++i) {
+                if (rating[i] < rating[j]) leftSmaller++;
+                else if (rating[i] > rating[j]) leftGreater++;
+            }
+
+            // Count elements on the right of j
+            for (int k = j + 1; k < n; ++k) {
+                if (rating[k] > rating[j]) rightGreater++;
+                else if (rating[k] < rating[j]) rightSmaller++;
+            }
+
+            count += (leftSmaller * rightGreater) + (leftGreater * rightSmaller);
+        }
+
+        return count;
     }
 };
