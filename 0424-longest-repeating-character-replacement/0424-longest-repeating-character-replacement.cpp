25504e08301ccefac641c324badc0a388a6a0351
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-19 14:05:05 -0700

    Time: 9 ms (38.31%), Space: 11.1 MB (21.1%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int ans = 0, maxCount = 0, j = 0;
-        unordered_map<char,int>m;
-        for(int i = 0 ; i < s.length() ; ++i){
+        unordered_map<char, int>m;
+        int i = 0, j = 0, maxCount = 0, answer = INT_MIN;
+        while(i<s.size()){
             m[s[i]]++;
             maxCount = max(maxCount, m[s[i]]);
-            while(i-j-maxCount+1>k){
+            while(j<s.size() && i-j-maxCount+1>k){
                 m[s[j]]--;
                 maxCount = max(maxCount, m[s[j]]);
                 ++j;
             }
-            ans = max(ans, i-j+1);
+            if(answer<i-j+1) answer = i-j+1;
+            ++i;
         }
-        return ans;
+        return answer;
     }
 };
