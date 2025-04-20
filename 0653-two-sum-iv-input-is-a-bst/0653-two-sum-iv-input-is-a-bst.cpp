111a466e3cec43116e0ffe8710bd8ca5ddf1e379
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-04-20 16:53:36 -0700

    Time: 0 ms (100%), Space: 37.1 MB (78.46%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,23 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    unordered_map<int,int>m;
+    void inOrder(TreeNode* root, vector<int>& nums) {
+        if (!root) return;
+        inOrder(root->left, nums);
+        nums.push_back(root->val);
+        inOrder(root->right, nums);
+    }
+
     bool findTarget(TreeNode* root, int k) {
-        if(!root) return false;
-        if(m[k-root->val]>0) return true;
-        m[root->val]++;
-        if(findTarget(root->left, k)) return true;
-        if(findTarget(root->right, k)) return true;
+        vector<int> nums;
+        inOrder(root, nums);  // Get sorted elements
+
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == k) return true;
+            else if (sum < k) ++left;
+            else --right;
+        }
         return false;
     }
 };
