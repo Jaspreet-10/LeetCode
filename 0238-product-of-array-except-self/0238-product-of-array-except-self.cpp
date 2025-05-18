6d4e82680390d370a6607f7a0574a859ac7100f4
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-05-17 17:48:59 -0700

    Time: 0 ms (100%), Space: 40.3 MB (58.62%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int countZero = 0, prod = 1;
-        for(int i = 0 ; i < nums.size() ; ++i){
-            if(nums[i] == 0) ++countZero;
-            else{
-                prod*=nums[i];
-            }
+        int prod = 1, count = 0, n = nums.size();
+        for(int i = 0 ; i < n ; ++i){
+            if(nums[i] == 0) ++count;
+            else
+            prod*=nums[i];
         }
-        if(countZero > 1){
-            vector<int>v(nums.size(),0);
+        if(count>1){
+            vector<int>v(n, 0);
             return v;
         }
-        for(int i = 0 ; i < nums.size(); ++i){
-            if(countZero == 1 && nums[i]!=0) nums[i] = 0;
-            else if(countZero == 1 && nums[i] == 0)
-                nums[i] = prod;
+        for(int i = 0 ; i < n ; ++i){
+            if(nums[i]!=0 and count == 1) nums[i] = 0;
+            else if(nums[i] == 0) nums[i] = prod;
             else{
                 nums[i] = prod/nums[i];
-            } 
+            }
         }
         return nums;
     }
