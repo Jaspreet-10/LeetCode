bab225a4f7b7c2252dd655c2ae70ebc6bb864949
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-05-20 14:13:12 -0700

    Time: 0 ms (100%), Space: 8.7 MB (99.03%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        unordered_map<char,int>m;
-        int mini = INT_MAX;
-        for(int i = 0 ; i < text.size() ; ++i){
-            if(text[i] == 'b' || text[i] == 'a' || text[i] == 'l' || text[i] == 'o' || text[i] == 'n'){
-                m[text[i]]++;
-            }
+         int i;
+        int b=0,a=0,l=0,o=0,n=0;
+
+        //loop to traverse over string 'text' increase the count of letters: b,a,l,o,n
+        for(i=0;i<text.size();i++)
+        {
+            if(text[i]=='b')
+                b++; 
+            else if(text[i]=='a')
+                a++;
+            else if(text[i]=='l')
+                l++;
+            else if(text[i]=='o')
+                o++;
+            else if(text[i]=='n')
+                n++;
         }
-        if(m.size()!=5) return 0;
-        for(auto it:m){
-            if(it.first == 'l' || it.first == 'o'){
-                mini = min(mini, it.second/2);
-            }else{
-                 mini = min(mini, it.second);
-            }
-        }
-        return mini;
+
+        //setting 'ans' to minimum occurence of these characters(it will be the minimum count of "balloon'")
+        int ans=min({b,a,l/2,o/2,n});
+        return ans;
     }
 };
