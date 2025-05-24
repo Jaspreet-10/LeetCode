02f96111c07cd801a0cc55067190bb5802e7c54a
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-05-23 17:16:02 -0700

    Time: 0 ms (100%), Space: 16.7 MB (87.66%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<pair<int, int>, int>>q;
-        int m = grid.size(), n = grid[0].size(), steps = 0;
-        for(int i = 0 ; i < m ; ++i){
-            for(int j = 0 ; j < n ; ++j){
-                if(grid[i][j] == 2){
-                    q.push({{i, j}, 0});
+        queue<pair<pair<int,int>, int>>q;
+        int steps = 0;
+        for(int i = 0 ; i < grid.size() ; ++i){
+            for(int j = 0 ; j < grid[i].size() ; ++j){
+            if(grid[i][j] == 2){
+                q.push({{i,j}, 0});
                 }
             }
         }
         while(!q.empty()){
-            int i = q.front().first.first;
-            int j = q.front().first.second;
+            int r = q.front().first.first;
+            int c = q.front().first.second;
             steps = q.front().second;
             q.pop();
-            int row[4] = {-1, 0, 1, 0};
-            int col[4] = {0, -1, 0, 1};
-            for(int k = 0 ; k < 4 ; ++k){
-                int rc = i+row[k];
-                int cc = j+col[k];
-                if(rc>=0 && cc>=0 && rc<m && cc<n &&
-                grid[rc][cc] == 1){
-                    grid[rc][cc] = -1;
-                    q.push({{rc,cc}, steps+1});
+            int row[4] = {1,0,-1,0};
+            int col[4] = {0,-1,0,1};
+            for(int i = 0 ; i < 4 ; ++i){
+                int rc = row[i] + r;
+                int cc = col[i] + c;
+                if(rc>=0 && cc>=0 && rc<grid.size() && 
+                cc<grid[0].size() && grid[rc][cc] == 1){
+                    q.push({{rc, cc}, steps+1});
+                    grid[rc][cc] = 0;
                 }
             }
         }
-        for(int i = 0 ; i < m ; ++i){
-            for(int j = 0 ; j < n ; ++j){
+        for(int i = 0 ; i < grid.size() ; ++i){
+            for(int j = 0 ; j < grid[i].size() ; ++j){
                 if(grid[i][j] == 1) return -1;
             }
         }
