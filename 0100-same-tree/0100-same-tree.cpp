25477ee355f46185215cc5382c627a637487a15e
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-06-30 14:18:54 -0700

    Time: 0 ms (100%), Space: 12.9 MB (47.85%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,17 +11,17 @@
  */
 class Solution {
 public:
-    bool isSameTreeHelper(TreeNode* p, TreeNode* q){
+    bool helper(TreeNode* p, TreeNode* q){
         if(!p && !q) return true;
-        if(!p || !q) return false;
+        if((!p && q) || (!q and p)) return false;
 
         if(p->val == q->val){
-            return isSameTreeHelper(p->left, q->left) and 
-            isSameTreeHelper(p->right, q->right);
+            return helper(p->left, q->left) && helper(p->right, q->right);
         }else return false;
-        return true;
+        return false;
     }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        return isSameTreeHelper(p,q);
+        if((!p && q) || (!q and p)) return false;
+        return helper(p, q);
     }
 };
