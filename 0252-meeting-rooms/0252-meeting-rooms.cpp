757b69dbb6ca004c042505799b9fa539a18a7ca4
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-08-03 10:49:50 -0700

    Time: 0 ms (100%), Space: 16.1 MB (20.75%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
+        // sort(intervals.begin(), intervals.end());
         if(intervals.size()<1) return true;
        vector<pair<int,int>>v;
         for(int i = 0 ; i < intervals.size() ; ++i){
             v.push_back({intervals[i][1], intervals[i][0]});
         }
         sort(v.begin(),v.end());
-        int e = v[0].first, count = 1;
+        int e = v[0].first;
         for(int i = 1 ; i < intervals.size() ; ++i){
             if(e<=v[i].second){
                 e = v[i].first;
-            }else{
-                ++count;
-            }
+            }else return false;
         }
-        return count;
+        return true;
     }
 };
