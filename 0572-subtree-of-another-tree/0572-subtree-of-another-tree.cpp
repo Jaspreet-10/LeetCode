0bbc0980ba3969fa7123862baa6a55125f5abc93
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-08-24 13:39:35 -0700

    Time: 0 ms (100%), Space: 29.2 MB (14.68%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -12,17 +12,16 @@
 class Solution {
 public:
     bool helper(TreeNode* p, TreeNode* q){
-        if(!p && !q) return true;
-        if((!p && q) || (!q and p)) return false;
-
+        if(!p and !q) return true;
+        if((!p and q) or (p and !q)) return false;
         if(p->val == q->val){
-            return helper(p->left, q->left) && helper(p->right, q->right);
+            return helper(p->left, q->left) and helper(p->right, q->right);
         }else return false;
         return true;
     }
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         if(!root || !subRoot) return false;
-        if(root->val == subRoot->val){
+        if(subRoot->val == root->val){
             if(helper(root, subRoot)) return true;
         }
         return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
