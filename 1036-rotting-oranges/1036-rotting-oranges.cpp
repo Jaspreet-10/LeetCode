5a3d5cb68e06e38c285cd4c70467ea94628d715d
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-08-24 18:20:40 -0700

    Time: 0 ms (100%), Space: 16.8 MB (75.22%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -3,7 +3,6 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size(), dist = 0;
         queue<pair<pair<int,int>, int>>q;
-        vector<vector<int>>vis(m, vector<int>(n, 0));
         for(int i = 0 ; i < m ; ++i){
             for(int j = 0 ; j < n ; ++j){
                 if(grid[i][j] == 2) q.push({{i, j}, 0});
