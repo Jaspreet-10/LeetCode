fcaa1166eda8ddc4c3f02627e6faf15a96b12bb5
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-09-08 18:38:17 -0600

    Time: 7 ms (20.34%), Space: 9.7 MB (74.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int>tmp(26,0);
-        if(s.size()!=t.size()) return false;
-        for(int i = 0 ; i < s.size() ; ++i){
-            tmp[s[i] - 'a']++;
-            tmp[t[i] - 'a']--;
-        }
-        for(int i = 0 ; i < tmp.size() ; ++i){
-            if(tmp[i]!=0) return false;
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s == t) return true;
+        return false;
     }
 };
