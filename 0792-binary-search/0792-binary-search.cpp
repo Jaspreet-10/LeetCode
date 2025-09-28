b814fd0921f507546cf7603c3e32899d6665e53c
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-09-28 14:15:44 -0600

    Time: 0 ms (100%), Space: 31.4 MB (9.51%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0, high = nums.size();
-        while(low<high){
+        int low = 0, high = nums.size()-1;
+        while(low<=high){
             int mid = (low+high)/2;
             if(nums[mid] == target) return mid;
             if(nums[mid]>target)
-                high = mid;
+                high = mid-1;
             else 
                 low = mid+1;
         }
