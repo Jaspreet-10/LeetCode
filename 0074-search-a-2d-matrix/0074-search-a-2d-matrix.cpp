9ec077a083d95ece82da431dd8c12b90ea41e30b
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2025-09-28 14:25:29 -0600

    Time: 0 ms (100%), Space: 13.4 MB (7.42%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool findElement(vector<vector<int>>&matrix, int target, int row){
+    bool findRowElement(vector<vector<int>>&matrix, int target, int row){
         int low = 0, high = matrix[row].size()-1;
         while(low<=high){
             int mid = (low+high)/2;
@@ -13,13 +13,24 @@ public:
         }
         return false;
     }
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int col = matrix[0].size();
-        for(int i = 0 ; i < matrix.size() ; ++i){
-            if(matrix[i][col-1]>=target){
-                return findElement(matrix,target,i);
+
+     bool findColElement(vector<vector<int>>&matrix, int target, int col){
+        int low = 0, high = matrix.size()-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(target>=matrix[mid][0] and target<=matrix[mid][col]) 
+                return findRowElement(matrix, target, mid);
+            else if(matrix[mid][col]>target){
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
         return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int col = matrix[0].size()-1;
+        return findColElement(matrix, target, col);
     }
 };
