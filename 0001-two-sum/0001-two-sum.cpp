b73d46d545ed4753068914cb89cf0ad522f1dc9e
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-01-03 17:15:44 -0800

    Time: 0 ms (100%), Space: 14.9 MB (37.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>m;
+        unordered_map<int, int>m;
         for(int i = 0 ; i < nums.size() ; ++i){
-            if(m[target-nums[i]]>0){
-                return {m[target-nums[i]]-1,i};
-            }
-            m[nums[i]] = i+1;
+            if(m.find(target-nums[i])!=m.end()) return {m[target-nums[i]], i};
+            m[nums[i]] = i;
         }
-        return {};
+        return {0,0};
     }
 };
-
-/*
-Ask question, is the array sorted and do they have the negative values in the array(map approach will work with negative values as well)
-First apply brute force apporach which will take - O(N^2), second is sort the array as and apply two pointers, at the end apply the unordered map approach*/
