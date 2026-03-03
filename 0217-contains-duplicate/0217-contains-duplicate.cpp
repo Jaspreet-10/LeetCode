7f531acd6892f2d1496c248abfbeffdb137eb7c1
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-03-03 11:41:29 -0800

    Time: 53 ms (36.56%), Space: 90.8 MB (58.96%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>m;//Dictionary [1,2,3,1]
+        unordered_map<int, int>m;
         for(int i = 0 ; i < nums.size() ; ++i){
-            if(m[nums[i]]>0) return true; //m[1] > 0
-            m[nums[i]]+=1; //1->1 m[1] = 1, m[2] = 1, m[3] = 1
+            if(m.find(nums[i])!=m.end()) return true;
+            m[nums[i]]++;
         }
-    return false;
+        return false;
     }
 };
