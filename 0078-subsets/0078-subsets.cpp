75485867975da9350a7307311c1534b654b23237
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-08-03 15:31:19 -0700

    Time: 255 ms (3.56%), Space: 109.8 MB (5.04%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    void findSubsets(int index, vector<int>&nums, vector<vector<int>>&ans, int n,
-        unordered_map<int,int>&m, vector<int>&temp){
-            if(index>=n){
-                ans.push_back(temp);
-                return ;
-            }
-            temp.push_back(nums[index]);
-            findSubsets(index+1, nums, ans, n, m, temp);
-            temp.pop_back();
-            findSubsets(index+1, nums, ans, n, m, temp);
-            return;
+    vector<vector<int>>findSubsets(int index, vector<int>&nums, vector<int>temp, 
+    vector<vector<int>>&ans){
+        if(index>=nums.size()){
+            ans.push_back(temp);
+            return ans;
         }
+        temp.push_back(nums[index]);
+        findSubsets(index+1, nums, temp, ans);
+        temp.pop_back();
+        findSubsets(index+1, nums, temp, ans);
+        return ans;
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>ans;
-        unordered_map<int, int>m;
         vector<int>temp;
-        findSubsets(0, nums, ans,  nums.size(), m, temp);
-        return ans;
+        return findSubsets(0, nums, temp, ans);
     }
 };
