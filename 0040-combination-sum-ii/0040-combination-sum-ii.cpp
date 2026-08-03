6cb1543b459bba4e06e4073509b4f41f6251ff3f
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-08-03 16:24:45 -0700

    Time: 0 ms (100%), Space: 14.1 MB (65.44%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void findTarget(int index, vector<int>& candidates, int n, int target, vector<int>&temp, map<vector<int>, int>&m, vector<vector<int>>&ans){
+    void findTarget(int index, vector<int>& candidates, int n, int target, vector<int>&temp,  vector<vector<int>>&ans){
         if(target == 0){
             ans.push_back(temp);
             return ;
@@ -8,21 +8,20 @@ public:
         if(index>=candidates.size()) return ;
         if(candidates[index]<=target){
         temp.push_back(candidates[index]);
-        findTarget(index+1, candidates, n, target-candidates[index], temp, m, ans);
+        findTarget(index+1, candidates, n, target-candidates[index], temp, ans);
         temp.pop_back();
         while (index != candidates.size() - 1 && candidates[index] == candidates[index + 1]){
             index++;
         }
         }
-        findTarget(index+1, candidates, n, target,temp, m, ans);
+        findTarget(index+1, candidates, n, target,temp, ans);
         return ;
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
         vector<int>temp;
-        map<vector<int>, int>m;
         sort(candidates.begin(), candidates.end());
-        findTarget(0, candidates, candidates.size(), target, temp, m, ans);
+        findTarget(0, candidates, candidates.size(), target, temp, ans);
         return ans;
     }
 };
