c912f0ad143e84b41490fa7dd30ce7ac5068b17f
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-08-03 15:49:36 -0700

    Time: 4 ms (37.27%), Space: 14.2 MB (59.8%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    vector<vector<int>>findTarget(int index, vector<int>& candidates, int target, vector<int>temp, vector<vector<int>>&ans){
-        if(index>=candidates.size() || target<0) return ans;
+    void findTarget(int index, vector<int>& candidates, int target, vector<int>&temp, vector<vector<int>>&ans){
+        if(index>=candidates.size()) return ;
         if(target == 0){
             ans.push_back(temp);
-            return ans;
+            return ;
         }
         if(candidates[index]<=target){
         temp.push_back(candidates[index]);
@@ -12,11 +12,12 @@ public:
         temp.pop_back();
         }
         findTarget(index+1, candidates, target,temp, ans);
-        return ans;
+        return ;
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
         vector<int>temp;
-        return findTarget(0, candidates, target, temp, ans);
+        findTarget(0, candidates, target, temp, ans);
+        return ans;
     }
 };
