334646bcc3219d1421cd111c63134c939eb3bf9b
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-08-04 17:01:05 -0700

    Time: 1 ms (83.28%), Space: 18.6 MB (95.95%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    bool dfs(int node, vector<int>adj[], vector<int>&vis, vector<int>visPath){
-        if(vis[node]!=0) return false;
-        vis[node] = 1;
-        visPath[node] = 1;
-        for(auto it : adj[node]){
-            if(vis[it]!=0 and visPath[it]!=0) return true;
-            if(dfs(it, adj, vis, visPath)) return true;
-            visPath[it] = 0;
-        }
-        return false;
-    }
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<int>vis(n, 0);
-        vector<int>visPath(n, 0);
+        vector<int>inDegree(n, 0);
         vector<int>adj[n];
+        queue<int>q;
         for(int i = 0 ; i < pre.size() ; ++i){
             adj[pre[i][1]].push_back(pre[i][0]);
         }
         for(int i = 0 ; i < n ; ++i){
-            if(vis[i] == 0){
-                if(dfs(i, adj, vis, visPath)) return false;
+            for(auto it : adj[i]){
+                inDegree[it]++;
             }
         }
+        for(int i = 0 ; i < n ; ++i) if(inDegree[i] == 0) q.push(i);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(int i = 0 ; i < adj[node].size() ; ++i){
+                inDegree[adj[node][i]]--;
+                if(inDegree[adj[node][i]] == 0) q.push(adj[node][i]);
+            }
+        }
+        for(int i = 0 ; i < n ; ++i)
+            if(inDegree[i]!=0) return false;
         return true;
     }
 };
