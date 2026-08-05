073ab1047ff422e2c16d44b1bc3bc5a129b9f12e
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-08-04 18:09:13 -0700

    Time: 0 ms (100%), Space: 71.8 MB (53.51%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,7 +4,7 @@ public:
         int sum = 0, maxi = INT_MIN;
         for(int i = 0 ; i < nums.size() ; ++i){
             sum+=nums[i];
-            if(sum>maxi) maxi = sum;
+            maxi = max(sum, maxi);
             if(sum<0) sum = 0;
         }
         return maxi;
