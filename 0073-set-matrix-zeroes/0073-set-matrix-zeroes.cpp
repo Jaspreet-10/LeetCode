b959a07f87ded8b25c168da2a77cfd9d05a2aebb
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-08-05 22:10:36 -0700

    Time: 0 ms (100%), Space: 21.1 MB (21.78%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_map<int,int>mapRow, mapCol;
-        for(int i = 0 ; i < matrix.size() ; ++i){
-            for(int j = 0 ; j < matrix[i].size() ; ++j){
-                if(matrix[i][j] == 0){
-                    mapRow[i]++;
-                    mapCol[j]++;
-                } 
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int col0 = 1; // Indicates whether the first column should be zeroed
+
+        // Step 1: Use first row and first column as markers
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0)
+                col0 = 0;
+
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
             }
         }
-        for(int i = 0 ; i < matrix.size() ; ++i){
-            for(int j = 0 ; j < matrix[i].size() ; ++j){
-                if(mapRow[i]>0 || mapCol[j]>0){
+
+        // Step 2: Update the matrix in reverse order
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
                     matrix[i][j] = 0;
-                } 
             }
+
+            if (col0 == 0)
+                matrix[i][0] = 0;
         }
-        return;
-    }
-};
+        }
+    };
