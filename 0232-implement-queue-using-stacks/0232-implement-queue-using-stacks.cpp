626ca1df038f0cd56b753bae99a7068e2d146c11
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-08-12 23:45:14 -0700

    Time: 0 ms (100%), Space: 10.2 MB (48.37%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -6,43 +6,30 @@ public:
     }
     
     void push(int x) {
+        while(!st1.empty()){
+            st2.push(st1.top());
+            st1.pop();
+        }
         st1.push(x);
+        while(!st2.empty()){
+            st1.push(st2.top());
+            st2.pop();
+        }
     }
     
     int pop() {
-       int t;
-       if(!st2.empty()){
-        t = st2.top();
-        st2.pop();
-       }
-       else{
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
-        }
-        t = st2.top();
-        st2.pop();
-       }
-        return t;
+       int t = st1.top();
+       st1.pop();
+       return t;
     }
     
     int peek() {
-        int t;
-       if(!st2.empty()){
-        t = st2.top();
-       }
-       else{
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
-        }
-        t = st2.top();
-       }
-        return t;
+        return st1.top();
     }
     
     bool empty() {
-        return st1.empty() && st2.empty();
+        if(!st1.empty()) return false;
+        return true;
     }
 };
 
